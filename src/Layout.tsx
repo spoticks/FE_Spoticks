ffd92fb746e366483d029d4c67d8cf830acd4f93
dohyeons <ddhhss0603@gmail.com>
2024-07-30 13:09:17 +0900

    ✨ feat: 임시 헤더 구현 #1

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -4,8 +4,9 @@ import vector from "./assets/Vector.svg";
 export default function Layout() {
   return (
     <div>
+      <header className="fixed w-full border border-black">헤더</header>
       <Outlet />
-      <img src={vector} className="left- fixed top-[-20px] -z-50" />
+      <img src={vector} className="fixed left-0 top-[-20px] -z-50" />
     </div>
   );
 }
