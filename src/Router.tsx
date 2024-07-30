2c731956cb9d93c636708a7314148ba5a02a37dd
dohyeons <ddhhss0603@gmail.com>
2024-07-30 13:54:03 +0900

    ✨ feat: 기본 라우터 설정 #2

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,6 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "./Home";
+import Home from "./pages/Home";
 import Layout from "./Layout";
+import SignUp from "./pages/SignUp";
+import Login from "./pages/Login";
+import MyPage from "./pages/Mypage";
+import Reservation from "./pages/Reservation";
+import MatchList from "./pages/MatchList";
+import Admin from "./pages/Admin";
+import NotFound from "./pages/NotFound";
 
 export default function Router() {
   return (
@@ -8,6 +15,13 @@ export default function Router() {
       <Routes>
         <Route element={<Layout />}>
           <Route path="/" element={<Home />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/match-list" element={<MatchList />} />
+          <Route path="/my-page" element={<MyPage />} />
+          <Route path="/reservation" element={<Reservation />} />
+          <Route path="/admin" element={<Admin />} />
+          <Route path="*" element={<NotFound />} />
         </Route>
       </Routes>
     </BrowserRouter>
