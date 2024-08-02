05fe995d9d157af8c4d76edfa5558671a018df3b
dohyeons <ddhhss0603@gmail.com>
2024-08-02 12:13:13 +0900

    💄 design: 메인 레이아웃에 padding을 Header의 높이만큼 추가 #7

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -6,7 +6,7 @@ export default function Layout() {
   return (
     <div className="flex min-h-screen flex-col items-center">
       <Header />
-      <div className="flex min-w-[1280px] flex-1 flex-col items-center pt-6">
+      <div className="flex min-w-[1280px] flex-1 flex-col items-center pt-[80px]">
         <Outlet />
         <img src={vector} className="fixed left-0 top-[-20px] -z-50" />
       </div>
