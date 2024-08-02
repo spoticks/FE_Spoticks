ea0074464e026cc84dc1f040d501c622e6c48207
dohyeons <ddhhss0603@gmail.com>
2024-08-02 12:04:21 +0900

    🐛  fix: ButtonProps 인터페이스 수정 #7

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,30 +1,26 @@
 interface ButtonProps {
-  w: string;
-  h: string;
   content: string;
 }
 
-const Button = ({ content }:ButtonProps) => {
-  const getStyles = (content:string) => {
+const Button = ({ content }: ButtonProps) => {
+  const getStyles = (content: string) => {
     switch (content) {
       case "로그인":
         return {
           base: "w-100 h-40 bg-foreground text-text-primary border-text-primary",
-          hover:
-            "hover:bg-Accent hover:text-foreground hover:border-Accent",
+          hover: "hover:bg-Accent hover:text-foreground hover:border-Accent",
         };
 
       case "회원가입":
         return {
           base: "w-100 h-40 bg-Accent text-foreground border-Accent",
-          hover:
-            "hover:bg-[#a50115] hover:text-foreground hover:border-[#a50115]",
+          hover: "hover:bg-[#a50115] hover:text-foreground hover:border-[#a50115]",
         };
-      case 'block':
-          return {
-            base: "bg-[#dbdbdb] text-[#222222] border-[#dbdbdb]"
-          }
-      
+      case "block":
+        return {
+          base: "bg-[#dbdbdb] text-[#222222] border-[#dbdbdb]",
+        };
+
       default:
         return {
           base: "bg-defaultBg text-defaultText border-defaultBorder",
@@ -34,10 +30,10 @@ const Button = ({ content }:ButtonProps) => {
     }
   };
   const styles = getStyles(content);
-  
+
   return (
     <div
-      className={`${styles.base} border ${styles.hover} rounded-[10px] transition-all duration-300 flex justify-center items-center`}
+      className={`${styles.base} border ${styles.hover} flex items-center justify-center rounded-[10px] transition-all duration-300`}
     >
       {content}
     </div>
