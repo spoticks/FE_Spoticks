86fbebd06e724d5c3a02b7fab66ba9788e1f6802
dohyeons <ddhhss0603@gmail.com>
2024-08-05 11:10:24 +0900

    ✨ feat: 풋터 프로토타입 구현 #7

---
+++ src/components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="flex h-[80px] w-full justify-center border">
+      <div className="flex w-[1280px] items-center">Footer</div>
+    </footer>
+  );
+}
