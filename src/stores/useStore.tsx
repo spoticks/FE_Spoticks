4da89253600b815a057c0488aefc9f746dbab866
devminoh <gmerit0116@naver.com>
2024-08-05 13:38:59 +0900

    ✨ feat: 경기등록페이지 store 연결 #8

--- src/stores/useStore.tsx
+++ src/stores/useStore.tsx
@@ -1,5 +1,28 @@
 import create from "zustand";
 
-const useStore = create((set) => ({}));
+// Match 타입 정의
+interface Match {
+  date: string;
+  gameStartTime: string;
+  homeTeamName: string;
+  awayTeamName: string;
+  sportName: string;
+  reserveLink: string;
+}
+
+interface storeState {
+  matches: Match[];
+  addMatch: (match:Match) => void;
+}
+
+const useStore = create<storeState>((set) => ({
+  matches: [
+    { date: '2024/08/01', gameStartTime: '18:00', homeTeamName: '삼성라이온즈', awayTeamName: '엔씨다이노스', sportName: '축구', reserveLink: '#' },
+    { date: '2024/08/02', gameStartTime: '19:00', homeTeamName: '삼성라이온즈', awayTeamName: '엔씨다이노스', sportName: '야구', reserveLink: '#' },
+    { date: '2024/08/03', gameStartTime: '20:00', homeTeamName: '삼성라이온즈', awayTeamName: '엔씨다이노스', sportName: '테니스', reserveLink: '#' },
+    { date: '2024/08/04', gameStartTime: '21:00', homeTeamName: '삼성라이온즈', awayTeamName: '엔씨다이노스', sportName: '배구', reserveLink: '#' }
+  ],
+  addMatch: (match:Match) => set((state)=> ({matches: [...state.matches, match]}))
+}));
 
 export default useStore;
