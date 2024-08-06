3c7c2ffd6909a34a8280b0f6d6559ea61a7527aa
devminoh <gmerit0116@naver.com>
2024-08-06 17:19:47 +0900

    🐛 fix match데이터에 Id 추가 #8

--- src/stores/useStore.tsx
+++ src/stores/useStore.tsx
@@ -1,7 +1,8 @@
-import create from "zustand";
+import { create } from "zustand";
 
 // Match 타입 정의
 interface Match {
+  id: number;
   date: string;
   gameStartTime: string;
   homeTeamName: string;
@@ -17,10 +18,10 @@ interface storeState {
 
 const useStore = create<storeState>((set) => ({
   matches: [
-    { date: '2024/08/01', gameStartTime: '18:00', homeTeamName: '삼성라이온즈', awayTeamName: '엔씨다이노스', sportName: '축구', reserveLink: '#' },
-    { date: '2024/08/02', gameStartTime: '19:00', homeTeamName: '삼성라이온즈', awayTeamName: '엔씨다이노스', sportName: '야구', reserveLink: '#' },
-    { date: '2024/08/03', gameStartTime: '20:00', homeTeamName: '삼성라이온즈', awayTeamName: '엔씨다이노스', sportName: '테니스', reserveLink: '#' },
-    { date: '2024/08/04', gameStartTime: '21:00', homeTeamName: '삼성라이온즈', awayTeamName: '엔씨다이노스', sportName: '배구', reserveLink: '#' }
+    { id:1, date: '2024/08/01', gameStartTime: '18:00', homeTeamName: '삼성라이온즈', awayTeamName: '엔씨다이노스', sportName: '야구', reserveLink: '/reservation' },
+    { id:2, date: '2024/08/02', gameStartTime: '19:00', homeTeamName: '삼성라이온즈', awayTeamName: '기아타이거즈', sportName: '야구', reserveLink: '#' },
+    { id:3, date: '2024/08/03', gameStartTime: '20:00', homeTeamName: '삼성라이온즈', awayTeamName: '엔씨다이노스', sportName: '농구', reserveLink: '#' },
+    { id:4, date: '2024/08/04', gameStartTime: '21:00', homeTeamName: '삼성라이온즈', awayTeamName: '엔씨다이노스', sportName: '배구', reserveLink: '#' }
   ],
   addMatch: (match:Match) => set((state)=> ({matches: [...state.matches, match]}))
 }));
