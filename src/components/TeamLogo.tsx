81ad09187eb1f4c61a0df6f262afed1835768e3b
dohyeons <ddhhss0603@gmail.com>
2024-08-09 13:59:59 +0900

    ✨ feat: 팀명 및 사용되는 컴포넌트에 따른 팀 로고 컴포넌트 구현 #8

---
+++ src/components/TeamLogo.tsx
@@ -0,0 +1,39 @@
+import hanhwa from "../assets/baseballTeam/hanhwa_eagles.svg";
+import lg from "../assets/baseballTeam/lg-twins.svg";
+import kt from "../assets/baseballTeam/kt-wiz.svg";
+import ssg from "../assets/baseballTeam/ssg-landers.svg";
+import nc from "../assets/baseballTeam/nc-dinos.svg";
+import doosan from "../assets/baseballTeam/doosan-bears.svg";
+import kia from "../assets/baseballTeam/kia-tigers.svg";
+import lotte from "../assets/baseballTeam/lotte-giants.svg";
+import kiwoom from "../assets/baseballTeam/kiwoom-heros.svg";
+import samsung from "../assets/baseballTeam/samsung-lions.svg";
+
+const teamLogos: Record<string, string> = {
+  "한화 이글스": hanhwa,
+  "LG 트윈스": lg,
+  "KT 위즈": kt,
+  "SSG 랜더스": ssg,
+  "NC 다이노스": nc,
+  "두산 베어스": doosan,
+  "KIA 타이거즈": kia,
+  "롯데 자이언츠": lotte,
+  "키움 히어로즈": kiwoom,
+  "삼성 라이온즈": samsung,
+};
+
+interface TeamLogoProps {
+  teamName: string;
+  size?: "matchCard" | "mainMatch";
+}
+
+export default function TeamLogo({ teamName, size = "matchCard" }: TeamLogoProps) {
+  const sizeClasses = {
+    matchCard: "size-10",
+    mainMatch: "size-14",
+  };
+
+  const logoSrc = teamLogos[teamName];
+
+  return <img src={logoSrc} alt={teamName} className={sizeClasses[size]} />;
+}
