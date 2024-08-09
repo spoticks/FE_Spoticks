1ca1cbc5d362392a8d77c72bd0931f54f1483366
dohyeons <ddhhss0603@gmail.com>
2024-08-09 14:11:31 +0900

    💄 design: Layout 의 콘텐츠 레이아웃 최소길이를 최대길이로 변경 #8

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -7,7 +7,7 @@ export default function Layout() {
   return (
     <div className="flex min-h-screen flex-col items-center">
       <Header />
-      <div className="flex min-w-[1280px] flex-1 flex-col items-center pt-[80px]">
+      <div className="flex max-w-[1280px] flex-1 flex-col items-center pt-[80px]">
         <Outlet />
         <img src={vector} className="fixed left-0 top-[-20px] -z-50" />
       </div>
