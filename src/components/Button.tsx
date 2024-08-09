a2aeccc95b208e54d4a3192dbfab2d689608bec2
dohyeons <ddhhss0603@gmail.com>
2024-08-09 13:59:12 +0900

    ✨ feat: Button 컴포넌트에 예매하기 추가 #8

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -20,7 +20,11 @@ const Button = ({ content }: ButtonProps) => {
         return {
           base: "bg-borders text-text-primary border-borders",
         };
-
+      case "예매하기":
+        return {
+          base: " bg-Accent text-foreground px-8 py-1 text-xs w-26",
+          hover: "hover:bg-button-hovered",
+        };
       default:
         return {
           base: "bg-defaultBg text-defaultText border-defaultBorder",
