175c8ad90e644a29e209c8486d5aae07b5eb5d75
dohyeons <ddhhss0603@gmail.com>
2024-08-19 23:55:28 +0900

    ✨ feat: Header의 프로필 아이콘 조건부 렌더링 임시 구현 #8

--- src/components/Header.tsx/Header.tsx
+++ src/components/Header.tsx/Header.tsx
@@ -3,92 +3,85 @@ import Button from "../Button";
 import Logo from "../Logo";
 import HeaderNav from "./HeaderNav";
 import { LuUserCircle } from "react-icons/lu";
-import { useEffect, useRef, useState } from "react";
 import { FiFileText, FiLogOut, FiUser } from "react-icons/fi";
+import usePopover from "../../hooks/usePopover";
+import useAuthStore from "../../stores/authStore";
 
 export default function Header() {
-  const [isPopoverOpen, setIsPopoverOpen] = useState(false);
+  const { handleClickProfile, popoverRef, isPopoverOpen, setIsPopoverOpen } = usePopover();
+  const { isLoggedIn, username, accessToken, logout } = useAuthStore((state) => ({
+    isLoggedIn: state.isLoggedIn,
+    username: state.userName,
+    accessToken: state.accessToken,
+    logout: state.logout,
+  }));
 
-  function handleClickProfile(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
-    e.stopPropagation();
-    setIsPopoverOpen(!isPopoverOpen);
+  function handleLogout() {
+    logout();
+    localStorage.removeItem("accessToken");
+    localStorage.removeItem("username");
+    setIsPopoverOpen(false);
+    // 로그아웃 후 페이지 새로고침 또는 리다이렉트하기... 여기서는 리로드 함.
+    window.location.reload();
   }
-  const popoverRef = useRef<HTMLDivElement>(null);
-
-  useEffect(() => {
-    const handleClickOutside = (event: MouseEvent) => {
-      if (popoverRef.current && !popoverRef.current.contains(event.target as Node)) {
-        setIsPopoverOpen(false);
-      }
-    };
-
-    if (isPopoverOpen) {
-      document.addEventListener("click", handleClickOutside);
-    } else {
-      document.removeEventListener("click", handleClickOutside);
-    }
-
-    return () => {
-      document.removeEventListener("click", handleClickOutside);
-    };
-  }, [isPopoverOpen]);
 
   return (
     <header className="fixed flex h-[80px] w-full justify-center border bg-foreground">
       <div className="flex w-[1280px] items-center">
         <Logo />
         <HeaderNav />
-        {/* <div className="flex items-center gap-6 text-[18px]">
-          <Link to={"/sign-up"}>
-            <Button content="회원가입" />
-          </Link>
-          <Link to={"/login"}>
-            <Button content="로그인" />
-          </Link>
-        </div> */}
-        <div className="relative flex">
-          <button onClick={handleClickProfile}>
-            <LuUserCircle className="size-5" />
-          </button>
-          {isPopoverOpen && (
-            <div
-              ref={popoverRef}
-              className="mt- absolute right-0 top-5 w-24 rounded-lg border border-borders bg-white shadow-lg"
-            >
-              <div className="gap-3 p-2">
-                <p className="text-center text-[14px] font-semibold">
-                  홍길동 <span className="font-normal">님</span>
-                </p>
-              </div>
-              <div className="border-t border-gray-200">
-                <Link
-                  to="/profile"
-                  className="flex cursor-pointer items-center p-3 text-[14px] hover:bg-gray-100"
-                >
-                  <FiUser className="mr-3" />
-                  프로필
-                </Link>
-                <Link
-                  to="/tickets"
-                  className="flex cursor-pointer items-center p-3 text-[14px] hover:bg-gray-100"
-                >
-                  <FiFileText className="mr-3" />
-                  예매한 티켓
-                </Link>
+        {!isLoggedIn ? (
+          <div className="flex items-center gap-6 text-[18px]">
+            <Link to={"/sign-up"}>
+              <Button content="회원가입" />
+            </Link>
+            <Link to={"/login"}>
+              <Button content="로그인" />
+            </Link>
+          </div>
+        ) : (
+          <div className="relative flex">
+            <button onClick={handleClickProfile}>
+              <LuUserCircle className="size-5" />
+            </button>
+            {isPopoverOpen && (
+              <div
+                ref={popoverRef}
+                className="mt- absolute right-0 top-5 w-24 rounded-lg border border-borders bg-white shadow-lg"
+              >
+                <div className="gap-3 p-2">
+                  <p className="text-center text-[14px] font-semibold">
+                    {username} <span className="font-normal">님</span>
+                  </p>
+                </div>
+                <div className="border-t border-gray-200">
+                  <Link
+                    to="/profile"
+                    className="flex cursor-pointer items-center p-3 text-[14px] hover:bg-gray-100"
+                  >
+                    <FiUser className="mr-3" />
+                    프로필
+                  </Link>
+                  <Link
+                    to="/tickets"
+                    className="flex cursor-pointer items-center p-3 text-[14px] hover:bg-gray-100"
+                  >
+                    <FiFileText className="mr-3" />
+                    예매한 티켓
+                  </Link>
 
-                <button
-                  onClick={() => {
-                    setIsPopoverOpen(false);
-                  }}
-                  className="flex w-full cursor-pointer items-center rounded-b-lg p-3 text-[14px] hover:bg-gray-100"
-                >
-                  <FiLogOut className="mr-3" />
-                  로그아웃
-                </button>
+                  <button
+                    onClick={handleLogout}
+                    className="flex w-full cursor-pointer items-center rounded-b-lg p-3 text-[14px] hover:bg-gray-100"
+                  >
+                    <FiLogOut className="mr-3" />
+                    로그아웃
+                  </button>
+                </div>
               </div>
-            </div>
-          )}
-        </div>
+            )}
+          </div>
+        )}
       </div>
     </header>
   );
