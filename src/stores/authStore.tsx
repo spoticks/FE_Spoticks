da41ac7e2be6ed9ca00cf3796e2158688fa23ee3
dohyeons <ddhhss0603@gmail.com>
2024-08-19 18:32:25 +0900

    ✨ feat: authStore 에 userName 추가 #8

--- src/stores/authStore.tsx
+++ src/stores/authStore.tsx
@@ -3,15 +3,18 @@ import { create } from "zustand";
 interface AuthState {
   isLoggedIn: boolean;
   accessToken: string | null;
-  login: (token: string) => void;
+  userName: string | null;
+  login: (token: string, name: string) => void;
   logout: () => void;
 }
 
 const useAuthStore = create<AuthState>((set) => ({
   isLoggedIn: false,
   accessToken: null,
-  login: (token: string) => set({ isLoggedIn: true, accessToken: token }),
-  logout: () => set({ isLoggedIn: false, accessToken: null }),
+  userName: null,
+  login: (token: string, name: string) =>
+    set({ isLoggedIn: true, accessToken: token, userName: name }),
+  logout: () => set({ isLoggedIn: false, accessToken: null, userName: null }),
 }));
 
 export default useAuthStore;
