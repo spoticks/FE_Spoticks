f8fa89ced9a0fe0e6a631776ca9b56362ffa9f2e
dohyeons <ddhhss0603@gmail.com>
2024-08-20 10:45:53 +0900

    ✨ feat: 프로필 라우팅에서 로그인이 되지 않았을 경우 로그인 페이지로 이동 구현 #8

---
+++ src/AuthRoute.tsx
@@ -0,0 +1,16 @@
+import { Navigate, Outlet } from "react-router-dom";
+import useAuthStore from "./stores/authStore";
+
+export default function AuthRoute() {
+  const { accessToken } = useAuthStore((state) => ({
+    accessToken: state.accessToken,
+  }));
+
+  // 사용자가 로그인되지 않았으면 로그인 페이지로 리다이렉트
+  if (!accessToken) {
+    return <Navigate to="/login" />;
+  }
+
+  // 로그인된 경우 해당 라우트를 렌더링
+  return <Outlet />;
+}
