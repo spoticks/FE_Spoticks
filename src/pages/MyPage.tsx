5723a2f894df6fcbe76b54a1d0c931ee5e896c3c
dohyeons <ddhhss0603@gmail.com>
2024-08-20 14:19:05 +0900

    ✨ feat: 회원정보 제목 렌더링 #8

--- src/pages/MyPage.tsx
+++ src/pages/MyPage.tsx
@@ -1,3 +1,11 @@
+import useAuthStore from "../stores/authStore";
+
 export default function MyPage() {
-  return <div>마이 페이지</div>;
+  const { userName } = useAuthStore((state) => ({ userName: state.userName }));
+  return (
+    <>
+      <h1 className="text-2xl font-bold">회원정보</h1>
+      <span className="text-[#8d8d8d]">어서오세요 {userName}님</span>
+    </>
+  );
 }
