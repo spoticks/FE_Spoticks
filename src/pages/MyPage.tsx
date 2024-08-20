38d830df338952603f97a42a4bc1ba8f5867083e
dohyeons <ddhhss0603@gmail.com>
2024-08-21 00:11:35 +0900

    ✨ feat: PasswordSetting 렌더링 및 아이디 렌더링 #8

--- src/pages/MyPage.tsx
+++ src/pages/MyPage.tsx
@@ -1,5 +1,6 @@
 import useAuthStore from "../stores/authStore";
 import BasicUserInfo from "../components/UserInfo/BasicUserInfo";
+import PasswordSetting from "../components/UserInfo/PasswordSetting";
 
 export default function MyPage() {
   const { userName } = useAuthStore((state) => ({ userName: state.userName }));
@@ -8,10 +9,11 @@ export default function MyPage() {
     <>
       <div className="mb-10">
         <h1 className="text-2xl font-bold">회원정보</h1>
-        <span className="text-[#8d8d8d]">어서오세요 {userName}님</span>
+        <span className="text-[#8d8d8d]">어서오세요 {userName}(사용자 아이디) 님</span>
       </div>
       <div className="flex gap-40">
         <BasicUserInfo />
+        <PasswordSetting />
       </div>
     </>
   );
