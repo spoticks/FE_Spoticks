5d16156af5b71ed5c0f2d8df9c54e851efb4b8fd
dohyeons <ddhhss0603@gmail.com>
2024-08-21 00:09:09 +0900

    ✨ feat: BasicInfoField 에 input 타입을 바꿀 수 있도록 props 타입 추가 #8

--- src/components/UserInfo/BasicInfoField.tsx
+++ src/components/UserInfo/BasicInfoField.tsx
@@ -7,13 +7,14 @@ interface FormInputFieldProps {
   label: string;
   register: UseFormRegisterReturn;
   error?: FieldError | undefined;
+  type?: "password";
 }
 
-export default function BasicInfoField({ label, register, error }: FormInputFieldProps) {
+export default function BasicInfoField({ label, register, error, type }: FormInputFieldProps) {
   return (
     <div className="mb-4">
       <InputLabel label={label} />
-      <Input label={label} register={register} />
+      <Input label={label} register={register} type={type} />
       <InputErrorMessage error={error} />
     </div>
   );
