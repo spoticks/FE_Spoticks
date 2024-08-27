fbad0c98d1c124040d218fbf1c968a238ff57b0a
dohyeons <ddhhss0603@gmail.com>
2024-08-27 14:20:56 +0900

    💄 design: Loading 컴포넌트 가운데 정렬 #8

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,10 +1,10 @@
-import spinner from '../assets/spinner.svg';
+import spinner from "../assets/spinner.svg";
 
-export default function Loading () {
-  return(
-    <div>
+export default function Loading() {
+  return (
+    <div className="flex flex-col items-center">
       <h3>로딩중입니다.</h3>
       <img src={spinner} alt="loading" />
     </div>
-  )
+  );
 }
