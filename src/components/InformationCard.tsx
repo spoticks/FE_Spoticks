e222ead0c1e0524b892804781661a2d532a33895
dohyeons <ddhhss0603@gmail.com>
2024-08-27 14:20:39 +0900

    ✨ feat: 시간 렌더링 구현 #8

--- src/components/InformationCard.tsx
+++ src/components/InformationCard.tsx
@@ -1,17 +1,15 @@
 import { BsCalendar2EventFill } from "react-icons/bs";
 import { MdLocationOn } from "react-icons/md";
-interface InformationCardProp {
-  homeTeamName: string;
-  awayTeamName: string;
-  gameStartTime: string;
-  stadiumName: string;
-  reservationStatus?: boolean;
-}
+import { InformationCardProp } from "../type";
+
 export default function InformationCard({
   content: { homeTeamName, awayTeamName, gameStartTime, stadiumName, reservationStatus },
 }: {
   content: InformationCardProp;
 }) {
+  const [date, time] = gameStartTime.split("T");
+  const [hours, minutes] = time.split(":");
+
   return (
     <div className="w-[400px] rounded-[15px] border border-borders bg-foreground shadow-first">
       <div className="p-[20px]">
@@ -22,8 +20,10 @@ export default function InformationCard({
           <div className="mt-3 flex items-center gap-3">
             <BsCalendar2EventFill className="size-4" />
             <div className="flex flex-col text-xs">
-              <span className="">{gameStartTime}</span>
-              <span className="text-text-tertiary">{gameStartTime}</span>
+              <span className="">{date}</span>
+              <span className="text-text-tertiary">
+                {hours}:{minutes}
+              </span>
             </div>
           </div>
           <div className="mt-1 flex items-center gap-3">
