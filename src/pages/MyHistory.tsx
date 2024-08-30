25cca0fac4f5d3837704fdc0e5481a1dd706c520
dohyeons <ddhhss0603@gmail.com>
2024-08-30 14:14:46 +0900

    🚚  rename: MyReservation 컴포넌트 이름을 MyHistory 컴포넌트로 변경 #8

--- src/pages/MyHistory.tsx
+++ src/pages/MyHistory.tsx
@@ -4,16 +4,20 @@ import Loading from "../components/Loading";
 import Error from "./Error";
 import InformationCard from "../components/InformationCard";
 import { InformationCardProp } from "../type";
+import { useLocation } from "react-router-dom";
 
-export default function MyReservations() {
+export default function MyHistory() {
+  const location = useLocation().pathname;
+  const param = location === "/profile/my-tickets/my-reservations" ? "COMPLETE" : "CANCEL";
   const {
     data = [],
     isLoading,
     isError,
   } = useQuery<InformationCardProp[]>({
-    queryKey: ["myReservations"],
+    queryKey: ["myReservations", param],
     queryFn: async () => {
-      const res = await axios("http://localhost:3000/reservations");
+      // 추후에 reservationStatus를 status로 바꿀 것.
+      const res = await axios(`http://localhost:3000/reservations/?reservationStatus=${param}`);
       return res.data;
     },
   });
