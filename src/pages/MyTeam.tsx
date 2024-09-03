53e7ae5e8b1f50c62e7e2fe2bce5d017f3f35058
dohyeons <ddhhss0603@gmail.com>
2024-09-04 00:36:06 +0900

    ✨ feat: MyTeam 페이지 구현 #8

--- src/pages/MyTeam.tsx
+++ src/pages/MyTeam.tsx
@@ -1,3 +1,22 @@
+import useAuthStore from "../stores/authStore";
+import MyTeamList from "../components/MyTeamList";
+
+interface MyTeam {
+  teamId: number;
+  teamName: string;
+}
 export default function MyTeam() {
-  return <div>나의 팀</div>;
+  const { userName } = useAuthStore((state) => ({ userName: state.userName }));
+
+  return (
+    <>
+      <div className="mb-10 flex flex-col items-center border-b border-b-borders pb-2">
+        <h1 className="mb-2 text-2xl font-bold">My Team</h1>
+        <span className="text-text-tertiary">
+          {userName}님이 관심있는 팀들의 일정을 확인해보세요!
+        </span>
+      </div>
+      <MyTeamList />
+    </>
+  );
 }
