0c41e922395cf9dde4e1a679c15510a937f7f282
dohyeons <ddhhss0603@gmail.com>
2024-09-24 12:05:59 +0900

    🚚 rename: ReasonSelect를 컴포넌트 index로 변경 #42

---
+++ src/pages/AccountDeletion/AccountDeletionForm/ReasonSelect/index.tsx
@@ -0,0 +1,51 @@
+import InputLabel from "@/components/UserInfo/InputLabel";
+import usePopover from "@/hooks/usePopover";
+import DropdownButton from "@/pages/AccountDeletion/AccountDeletionForm/ReasonSelect/DropdownButton";
+import DropdownOptions from "@/pages/AccountDeletion/AccountDeletionForm/ReasonSelect/DropdownOptions";
+import { AccountDeletionFormType } from "@/type";
+import { Control, Controller } from "react-hook-form";
+
+export default function ReasonSelect({ control }: { control: Control<AccountDeletionFormType> }) {
+  const {
+    handleClickProfile: handleClickOptions,
+    isPopoverOpen: isOpen,
+    setIsPopoverOpen: setIsOpen,
+    popoverRef: optionRef,
+  } = usePopover();
+
+  return (
+    <>
+      <InputLabel label="무엇이 불편하셨나요?" />
+      <Controller
+        name="reason"
+        control={control}
+        rules={{
+          required: true,
+        }}
+        render={({ field }) => (
+          <div className="relative">
+            <DropdownButton
+              onClick={(e) => {
+                handleClickOptions(e);
+                if (isOpen) {
+                  field.onChange("");
+                }
+              }}
+              isOpen={isOpen}
+              label={field.value || "무엇이 불편하셨나요?"}
+            />
+            {isOpen && (
+              <DropdownOptions
+                onClick={(option) => {
+                  field.onChange(option);
+                  setIsOpen(false);
+                }}
+                optionRef={optionRef}
+              />
+            )}
+          </div>
+        )}
+      />
+    </>
+  );
+}
