5a3ae0bc8eb83c46842cb5a3b7c7e3a7eb51de90
dohyeons <ddhhss0603@gmail.com>
2024-09-26 17:10:55 +0900

    🚚 rename: AccountDeletion 페이지 폴더에 components 폴더 생성 후 컴포넌트 이동 #42

--- src/pages/AccountDeletion/components/AccountDeletionForm/ReasonSelect/index.tsx
+++ src/pages/AccountDeletion/components/AccountDeletionForm/ReasonSelect/index.tsx
@@ -1,7 +1,7 @@
 import InputLabel from "@/components/UserInfo/InputLabel";
 import usePopover from "@/hooks/usePopover";
-import DropdownButton from "@/pages/AccountDeletion/AccountDeletionForm/ReasonSelect/DropdownButton";
-import DropdownOptions from "@/pages/AccountDeletion/AccountDeletionForm/ReasonSelect/DropdownOptions";
+import DropdownButton from "@/pages/AccountDeletion/components/AccountDeletionForm/ReasonSelect/DropdownButton";
+import DropdownOptions from "@/pages/AccountDeletion/components/AccountDeletionForm/ReasonSelect/DropdownOptions";
 import { AccountDeletionFormType } from "@/type";
 import { Control, Controller } from "react-hook-form";
 
