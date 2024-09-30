0ca1e60c946c51ab45488539af9af808a9e03cd4
dohyeons <ddhhss0603@gmail.com>
2024-09-30 23:12:33 +0900

    ♻️ refactor: Input 사용되는 부분을 FormInputField로 리팩터링 #44

---
+++ src/common/components/molecules/FormInputField.tsx
@@ -0,0 +1,29 @@
+import Input from "@/common/components/atoms/Input";
+import InputErrorMessage from "@/common/components/atoms/InputErrorMessage";
+import InputLabel from "@/components/UserInfo/InputLabel";
+import { InputType } from "@/type";
+import { FieldError, UseFormRegisterReturn } from "react-hook-form";
+
+interface FormInputFieldProps {
+  label: string;
+  register: UseFormRegisterReturn;
+  error: FieldError | undefined;
+  isLabelRequired?: true;
+  inputType: InputType;
+}
+
+export default function FormInputField({
+  label,
+  register,
+  error,
+  isLabelRequired,
+  inputType,
+}: FormInputFieldProps) {
+  return (
+    <div>
+      {isLabelRequired && <InputLabel label={label} />}
+      <Input label={label} register={register} type={inputType} />
+      {error && <InputErrorMessage errorMessage={error.message} />}
+    </div>
+  );
+}
