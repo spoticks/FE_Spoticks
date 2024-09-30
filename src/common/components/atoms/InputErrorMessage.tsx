435a79635937665a8f8de3812091781c4f4f3ed0
dohyeons <ddhhss0603@gmail.com>
2024-09-30 23:25:54 +0900

    🚚 rename: InputErrorMessage의 props 속성에서 error를 errorMessage로 변경 #44

--- src/common/components/atoms/InputErrorMessage.tsx
+++ src/common/components/atoms/InputErrorMessage.tsx
@@ -1,13 +1,11 @@
-import { FieldError } from "react-hook-form";
-
-export default function InputErrorMessage({ error }: { error: FieldError | undefined }) {
+export default function InputErrorMessage({ errorMessage }: { errorMessage?: string }) {
   return (
     <div
       className={`transition-all duration-300 ease-in-out ${
-        error ? "max-h-10 opacity-100" : "max-h-0 opacity-0"
+        errorMessage ? "max-h-10 opacity-100" : "max-h-0 opacity-0"
       }`}
     >
-      {error && <p className="ml-2 text-[14px] text-Accent">{error.message}</p>}
+      <p className="ml-2 text-[14px] text-Accent">{errorMessage}</p>
     </div>
   );
 }
