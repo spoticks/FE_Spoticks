5115b3817f74e220a61a72ea15a41d0039634e92
dohyeons <ddhhss0603@gmail.com>
2024-10-08 17:34:42 +0900

    🚚 rename: SignUpForm 컴포넌트 분리 #52

---
+++ src/pages/SignUp/components/SignUpForm.tsx
@@ -0,0 +1,54 @@
+import Button from "@/common/components/atoms/Button";
+import FormInputField from "@/common/components/molecules/FormInputField";
+import { AuthFormType } from "@/common/types/formTypes";
+import onSubmitSignUpForm from "@/pages/SignUp/api/onSubmitSignUpForm";
+import { validationRules } from "@/validationRules";
+import { useForm } from "react-hook-form";
+
+export default function SignUpForm() {
+  const {
+    register,
+    formState: { errors, isValid },
+    handleSubmit,
+    getValues,
+  } = useForm<AuthFormType>({ mode: "onTouched" });
+
+  return (
+    <form className="my-4 flex flex-col" onSubmit={handleSubmit(onSubmitSignUpForm)}>
+      <FormInputField
+        label="이메일"
+        register={register("email", validationRules.email)}
+        error={errors.email}
+        inputType="email"
+      />
+      <FormInputField
+        label="이름"
+        register={register("name", validationRules.name)}
+        error={errors.name}
+        inputType="text"
+      />
+      <FormInputField
+        label="휴대전화"
+        register={register("phoneNumber", validationRules.phoneNumber)}
+        error={errors.phoneNumber}
+        inputType="text"
+      />
+      <FormInputField
+        label="비밀번호"
+        register={register("password", validationRules.password)}
+        error={errors.password}
+        inputType="password"
+      />
+      <FormInputField
+        label="비밀번호 재확인"
+        register={register("passwordConfirmation", {
+          ...validationRules.passwordConfirmation,
+          validate: (value) => validationRules.passwordConfirmation.validate(value, getValues),
+        })}
+        error={errors.passwordConfirmation}
+        inputType="password"
+      />
+      <Button content="회원가입 하기" isValid={isValid} />
+    </form>
+  );
+}
