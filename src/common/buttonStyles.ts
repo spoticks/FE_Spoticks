b129206c1e1542c3e18fdcb6039ad4318c63ecd3
dohyeons <ddhhss0603@gmail.com>
2024-10-16 23:41:08 +0900

    feat: button 스타일링을 위한 REDBUTTON_STYLE_AUTH 구현 #54

---
+++ src/common/buttonStyles.ts
@@ -0,0 +1,4 @@
+const REDBUTTON_STYLE_AUTH =
+  "mt-4 px-3 py-2 w-full rounded-[10px] bg-Accent text-[16px] text-foreground hover:bg-button-hovered disabled:cursor-not-allowed disabled:bg-disabled-button";
+
+export { REDBUTTON_STYLE_AUTH };
