399bf77b8b7bee0cdef0da1c20332e25db7cc66c
dohyeons <ddhhss0603@gmail.com>
2024-10-17 13:37:53 +0900

    ✨ feat: BASIC_RED_BUTTON_STYLE 스타일 추가 #54

--- src/common/buttonStyles.ts
+++ src/common/buttonStyles.ts
@@ -1,6 +1,8 @@
-const RED_BUTTON_STYLE_AUTH =
-  "mt-4 px-3 py-2 w-full rounded-[10px] bg-Accent text-[16px] text-foreground hover:bg-button-hovered disabled:cursor-not-allowed disabled:bg-disabled-button";
+const BASIC_RED_BUTTON_STYLE =
+  "px-3 py-2 w-full rounded-[10px] bg-Accent text-[16px] text-center text-foreground hover:bg-button-hovered disabled:cursor-not-allowed disabled:bg-disabled-button";
+
+const RED_BUTTON_STYLE_AUTH = "mt-4" + BASIC_RED_BUTTON_STYLE;
 
 const RED_TEXT_STYLE_AUTH = "text-Accent hover:text-button-hovered";
 
-export { RED_BUTTON_STYLE_AUTH, RED_TEXT_STYLE_AUTH };
+export { BASIC_RED_BUTTON_STYLE, RED_BUTTON_STYLE_AUTH, RED_TEXT_STYLE_AUTH };
