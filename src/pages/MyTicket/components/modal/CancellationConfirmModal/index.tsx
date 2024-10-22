228662467140620036f07b384aa70278853329a6
dohyeons <ddhhss0603@gmail.com>
2024-10-22 22:49:26 +0900

    ✨ feat: div를 h1으로 변경 #58

--- src/pages/MyTicket/components/modal/CancellationConfirmModal/index.tsx
+++ src/pages/MyTicket/components/modal/CancellationConfirmModal/index.tsx
@@ -19,7 +19,7 @@ export default function CancellationConfirmModal({
       className="fixed left-1/2 top-1/2 w-[394px] -translate-x-1/2 -translate-y-1/2 rounded-[20px] bg-foreground p-[40px] pb-[48px] shadow-second"
     >
       <section className="flex flex-col items-center">
-        <div className="mb-4 text-[20px] font-semibold">티켓을 취소하시겠습니까?</div>
+        <h1 className="mb-4 text-[20px] font-semibold">티켓을 취소하시겠습니까?</h1>
         <p className="mb-8 text-[14px] text-text-tertiary">{`티켓은 경기시작 1시간 전까지 취소 가능하며, 취소내역은 마이페이지 > 예매취소 에서 확인 가능합니다.`}</p>
         <div className="flex w-full justify-around">
           <BasicButton content="취소" style="text-[14px] font-semibold" onClick={onClose} />
