815449ea57926fcc05380bd4403b95accf855c9e
dohyeons <ddhhss0603@gmail.com>
2024-10-22 14:06:58 +0900

    ✨ feat: ModalHeader의 바닥 선 을 hr 태그로 변경 #58

--- src/common/components/molecules/ModalHeader.tsx
+++ src/common/components/molecules/ModalHeader.tsx
@@ -8,11 +8,14 @@ export default function ModalHeader({
   content: string;
 }) {
   return (
-    <header className="mb-5 flex w-full justify-between border-b border-b-borders pb-1">
-      <h1 className="font-semibold">{content}</h1>
-      <button onClick={onCloseButtonClick}>
-        <IoIosCloseCircle className="size-5 text-borders transition-all duration-150 hover:text-black" />
-      </button>
-    </header>
+    <>
+      <header className="flex w-full justify-between pb-1">
+        <h1 className="font-semibold">{content}</h1>
+        <button onClick={onCloseButtonClick}>
+          <IoIosCloseCircle className="size-5 text-borders transition-all duration-150 hover:text-black" />
+        </button>
+      </header>
+      <hr className="border-1 mb-5 w-full border-borders" />
+    </>
   );
 }
