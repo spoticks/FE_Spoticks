740e60735c6b1e01644b0a379d0cd0938d1e86e6
dohyeons <ddhhss0603@gmail.com>
2024-10-22 21:18:40 +0900

    💄 design: Loading 컴포넌트 중앙정렬 #58

--- src/common/components/atoms/Loading.tsx
+++ src/common/components/atoms/Loading.tsx
@@ -2,7 +2,7 @@ import Spinner from "@/assets/spinner.svg?react";
 
 export default function Loading() {
   return (
-    <div className="flex flex-col items-center">
+    <div className="flex h-full w-full flex-col items-center justify-center">
       <Spinner />
       <h3>로딩중입니다.</h3>
     </div>
