2b36e16bd2d52071067d32f1051775d50eb2b29e
dohyeons <ddhhss0603@gmail.com>
2024-10-24 11:40:47 +0900

    🔥 delete: console.log 삭제 #60

--- src/common/utils/axiosInstance.ts
+++ src/common/utils/axiosInstance.ts
@@ -5,7 +5,7 @@ const AxiosInstance = axios.create({
   baseURL: "http://spoticks.shop:8080/", // 서버 url
   timeout: 3000, // 3000ms가 지나도 응답이 없으면 요청을 취소
   headers: { "Content-Type": "application/json" }, // 클라에서 보내는 데이터의 형식
-  withCredentials: true,
+  withCredentials: true, // 요청에 credential 정보를 담아서 보낼 지
 });
 
 AxiosInstance.interceptors.request.use((config) => {
@@ -13,7 +13,6 @@ AxiosInstance.interceptors.request.use((config) => {
   if (accessToken) {
     config.headers.Authorization = accessToken;
   }
-  console.log(config);
   return config;
 });
 
