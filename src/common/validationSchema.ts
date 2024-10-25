014d52fce9cf5fbd450bc9e3c03f8562c8349ac1
dohyeons <ddhhss0603@gmail.com>
2024-10-25 15:03:29 +0900

    ✨ feat: 회원가입 폼 검증 스키마 구현 #62

--- src/common/validationSchema.ts
+++ src/common/validationSchema.ts
@@ -45,3 +45,11 @@ const fullSchema = z.object({
     invalid_type_error: "비밀번호 확인은 문자열이어야 합니다.",
   }),
 });
+
+export const signUpFormSchema = fullSchema.refine(
+  (data) => data.password === data.passwordConfirmation,
+  {
+    message: "비밀번호가 일치하지 않습니다.",
+    path: ["passwordConfirmation"],
+  },
+);
