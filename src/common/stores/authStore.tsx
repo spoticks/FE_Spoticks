2749323f95a4651085fd2f8965ee764888c3c304
dohyeons <ddhhss0603@gmail.com>
2024-10-25 16:16:51 +0900

    🔥 delete: isLoggedIn 상태 삭제 및 login에 이름 전달 삭제 #62

--- src/common/stores/authStore.tsx
+++ src/common/stores/authStore.tsx
@@ -5,18 +5,16 @@ interface AuthStateStore {
   id: string | null;
   userName: string | null;
   phoneNumber: string | null;
-  isLoggedIn: boolean;
   accessToken: string | null;
 }
 
 interface AuthStateActions {
-  login: (token: string, name: string) => void;
+  login: (token: string) => void;
   logout: () => void;
 }
 interface AuthStore extends AuthStateStore, AuthStateActions {}
 
 const initialState = {
-  isLoggedIn: false,
   accessToken: null,
   id: null,
   userName: null,
@@ -27,8 +25,7 @@ const useAuthStore = create<AuthStore>()(
   persist(
     (set) => ({
       ...initialState,
-      login: (token: string, name: string) =>
-        set({ isLoggedIn: true, accessToken: token, userName: name }),
+      login: (token: string) => set({ accessToken: token }),
       logout: () => set(() => initialState),
     }),
     { name: "auth-storage", storage: createJSONStorage(() => localStorage) },
