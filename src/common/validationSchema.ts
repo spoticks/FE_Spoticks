e7ed456004dea49e984226c30742d049a33771fd
dohyeons <ddhhss0603@gmail.com>
2024-10-25 16:06:06 +0900

    ✨ feat: 로그인 및 비밀번호 검증 구현 후 내보내기 #62

--- src/common/validationSchema.ts
+++ src/common/validationSchema.ts
@@ -2,10 +2,7 @@ import { z } from "zod";
 
 const fullSchema = z.object({
   username: z
-    .string({
-      required_error: "아이디를 입력해주세요.",
-      invalid_type_error: "아이디는 문자열이어야 합니다.",
-    })
+    .string()
     .min(5, { message: "아이디는 최소 5글자 이상이어야 합니다." })
     .max(25, { message: "아이디는 25글자 이하여야 합니다." })
     .regex(/^(?=.*[a-z])(?=.*\d)[a-z0-9]{5,25}$/, {
@@ -13,31 +10,22 @@ const fullSchema = z.object({
     }),
 
   name: z
-    .string({
-      required_error: "이름을 입력해주세요.",
-      invalid_type_error: "이름은 문자열이어야 합니다.",
-    })
+    .string()
     .min(2, { message: "이름은 두 글자 이상이어야 합니다." })
     .regex(/^[a-zA-Z가-힣]{2,}$/, {
       message: "이름은 두 글자 이상의 한글, 영문만 입력 가능합니다.",
     }),
 
-  phoneNumber: z
-    .string({
-      required_error: "전화번호를 입력해주세요.",
-      invalid_type_error: "전화번호는 문자열이어야 합니다.",
-    })
-    .regex(/^010-\d{4}-\d{4}$/, {
-      message: "유효한 번호를 입력해주세요. (예: 010-0000-0000)",
-    }),
+  phoneNumber: z.string().regex(/^010-\d{4}-\d{4}$/, {
+    message: "유효한 번호를 입력해주세요. (예: 010-0000-0000)",
+  }),
 
   password: z
-    .string({
-      required_error: "비밀번호를 입력해주세요.",
-      invalid_type_error: "비밀번호는 문자열이어야 합니다.",
-    })
-    .regex(/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/, {
-      message: "영문, 숫자, 특수문자 포함 최소 8자 이상이어야 합니다.",
+    .string()
+    .min(8, { message: "비밀번호는 최소 8글자 이상이어야 합니다." })
+    .max(20, { message: "비밀번호는 20글자 이하여야 합니다." })
+    .regex(/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,20}$/, {
+      message: "영문, 숫자, 특수문자 포함 최소 8~20글자여야 합니다.",
     }),
 
   passwordConfirmation: z.string({
@@ -51,10 +39,6 @@ const signUpFormSchema = fullSchema.refine((data) => data.password === data.pass
   path: ["passwordConfirmation"],
 });
 
-const phoneNumberSchema = fullSchema.pick({
-  phoneNumber: true,
-});
-
 const passwordSettingSchema = fullSchema
   .pick({
     password: true,
@@ -64,3 +48,14 @@ const passwordSettingSchema = fullSchema
     message: "비밀번호가 일치하지 않습니다.",
     path: ["passwordConfirmation"],
   });
+
+const loginFormSchema = fullSchema.pick({
+  username: true,
+  password: true,
+});
+
+const phoneNumberSchema = fullSchema.pick({
+  phoneNumber: true,
+});
+
+export { signUpFormSchema, passwordSettingSchema, loginFormSchema, phoneNumberSchema };
