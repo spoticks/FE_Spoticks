ba8b0c56d1905b3d0025f1a13df338cecd216ae8
dohyeons <ddhhss0603@gmail.com>
2024-10-25 16:18:45 +0900

    ✨ feat: 기본 로그인 구현 완성 #62

--- src/pages/Login/api/useLoginMutation.tsx
+++ src/pages/Login/api/useLoginMutation.tsx
@@ -1,11 +1,11 @@
 import useAuthStore from "@/common/stores/authStore";
+import axiosInstance from "@/common/utils/axiosInstance";
 import { useMutation } from "@tanstack/react-query";
-import axios from "axios";
 import { SubmitHandler } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
 
 interface LoginFormType {
-  email: string;
+  username: string;
   password: string;
 }
 
@@ -13,17 +13,12 @@ export default function useLoginMutation() {
   const navigate = useNavigate();
   const loginMutation = useMutation({
     // 로그인 로직인데 주소 및 기타 로직은 api 나오면 수정할 것
-    mutationFn: (loginData: LoginFormType) => axios.post("http://localhost:3000/login", loginData),
+    mutationFn: async ({ username, password }: { username: string; password: string }) => {
+      return await axiosInstance.post("auth/login", { username, password });
+    },
     onSuccess: (res) => {
-      // 본 로직
-      // const user = res.data[0];
-      // console.log(res);
-      // localStorage.setItem("accessToken", user.accessToken);
-      // localStorage.setItem("username", user.username);
-
-      // 임시 로직
-      localStorage.setItem("accessToken", "1234");
-      useAuthStore.getState().login("1234", "홍길동님");
+      const accessToken = res.data;
+      useAuthStore.getState().login(accessToken);
       navigate("/");
     },
     onError: (err) => {
