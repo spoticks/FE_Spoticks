faaa9011543a5f4f83c594171a843511793a47e7
devminoh <gmerit0116@naver.com>
2024-10-31 11:36:42 +0900

    ♻️ refactor: 빈태그 삭제 #63

--- src/pages/admin/components/DetailModal.tsx
+++ src/pages/admin/components/DetailModal.tsx
@@ -49,55 +49,53 @@ export default function DetailModal({ isOpen, onClose, match }: DetailModalProps
   };
 
   return (
-    <>
-      <Modal
-        isOpen={isOpen}
-        onRequestClose={onClose}
-        contentLabel="경기 상세 정보"
-        className="modal fixed inset-0 flex items-center justify-center"
-        overlayClassName="overlay fixed inset-0 bg-black bg-opacity-50"
-      >
-        <div className='px-[50px]" flex h-[550px] w-[500px] flex-col items-center justify-center rounded-[10px] bg-background pb-[30px] pt-[50px]'>
-          <header className="mb-4 flex w-[450px] flex-row justify-between">
-            <h1 className="text-2xl font-bold">경기 상세 정보</h1>
-            <button
-              onClick={onClose}
-              className="right-3 top-3 flex h-5 w-5 items-center justify-center rounded-full bg-borders text-xl text-background hover:text-text-primary"
-            >
-              x
-            </button>
-          </header>
-          <hr className="mb-4 w-[450px] border-t-2 border-borders" />
-          <main className="flex space-x-8">
-            <DetailInfo
-              details={Object.entries(Detail[0]).map(([key, value]) => ({
-                key,
-                value,
-                isAccent: key === "티켓오픈",
-              }))}
-            />
-            <DetailInfo
-              details={Object.entries(Detail[1]).map(([key, value]) => ({ key, value }))}
-              isRight
-            />
-          </main>
-          <div className="mt-4 flex justify-end space-x-11">
-            <button
-              onClick={handleDelete}
-              className="rounded-lg bg-text-tertiary px-8 py-2 text-background"
-            >
-              경기 삭제
-            </button>
-            <Link
-              to={`/admin/registration/${match.gameId}`}
-              state={{ mode: "edit", existMatch: match }}
-              className="rounded-lg bg-Accent px-8 py-2 text-background"
-            >
-              경기 수정
-            </Link>
-          </div>
+    <Modal
+      isOpen={isOpen}
+      onRequestClose={onClose}
+      contentLabel="경기 상세 정보"
+      className="modal fixed inset-0 flex items-center justify-center"
+      overlayClassName="overlay fixed inset-0 bg-black bg-opacity-50"
+    >
+      <div className='px-[50px]" flex h-[550px] w-[500px] flex-col items-center justify-center rounded-[10px] bg-background pb-[30px] pt-[50px]'>
+        <header className="mb-4 flex w-[450px] flex-row justify-between">
+          <h1 className="text-2xl font-bold">경기 상세 정보</h1>
+          <button
+            onClick={onClose}
+            className="right-3 top-3 flex h-5 w-5 items-center justify-center rounded-full bg-borders text-xl text-background hover:text-text-primary"
+          >
+            x
+          </button>
+        </header>
+        <hr className="mb-4 w-[450px] border-t-2 border-borders" />
+        <main className="flex space-x-8">
+          <DetailInfo
+            details={Object.entries(Detail[0]).map(([key, value]) => ({
+              key,
+              value,
+              isAccent: key === "티켓오픈",
+            }))}
+          />
+          <DetailInfo
+            details={Object.entries(Detail[1]).map(([key, value]) => ({ key, value }))}
+            isRight
+          />
+        </main>
+        <div className="mt-4 flex justify-end space-x-11">
+          <button
+            onClick={handleDelete}
+            className="rounded-lg bg-text-tertiary px-8 py-2 text-background"
+          >
+            경기 삭제
+          </button>
+          <Link
+            to={`/admin/registration/${match.gameId}`}
+            state={{ mode: "edit", existMatch: match }}
+            className="rounded-lg bg-Accent px-8 py-2 text-background"
+          >
+            경기 수정
+          </Link>
         </div>
-      </Modal>
-    </>
+      </div>
+    </Modal>
   );
 }
