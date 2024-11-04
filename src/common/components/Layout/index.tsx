8b6b47d24b4778693572d31ec8d59bae78cc9f5b
dohyeons <ddhhss0603@gmail.com>
2024-11-04 10:31:23 +0900

    ✨ feat: html 구조 변경 #67

--- src/common/components/Layout/index.tsx
+++ src/common/components/Layout/index.tsx
@@ -6,16 +6,14 @@ import Header from "@/common/components/Layout/Header/index";
 export default function Layout() {
   return (
     <div className="flex min-h-screen flex-col items-center">
-      <div className="w-[1280px]">
-        <Header />
-        <div className="flex w-full flex-1 flex-col items-center pt-[80px]">
-          <main className="w-full">
-            <Outlet />
-          </main>
-          <Vector className="fixed left-0 top-[-20px] -z-50" />
-        </div>
-        <Footer />
+      <Header />
+      <div className="flex w-[1280px] flex-1 flex-col items-center pt-[80px]">
+        <main className="w-full">
+          <Outlet />
+        </main>
+        <Vector className="fixed left-0 top-[-20px] -z-50" />
       </div>
+      <Footer />
     </div>
   );
 }
