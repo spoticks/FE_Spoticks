3b2bb8ec05470a77efa3beb6d289340bb2c8805e
dohyeons <ddhhss0603@gmail.com>
2024-11-05 17:48:51 +0900

    ✨ feat: 검증 rules를 schema로 변경 #68

--- src/pages/SignUp/components/SignUpForm.tsx
+++ src/pages/SignUp/components/SignUpForm.tsx
@@ -2,8 +2,9 @@ import { RED_BUTTON_STYLE_AUTH } from "@/common/buttonStyles";
 import BasicButton from "@/common/components/atoms/button/BasicButton";
 import FormInputField from "@/common/components/molecules/FormInputField";
 import { AuthFormType } from "@/common/types/formTypes";
-import validationRules from "@/common/validationRules";
+import { signUpFormSchema } from "@/common/validationSchema";
 import onSubmitSignUpForm from "@/pages/SignUp/api/onSubmitSignUpForm";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 
 export default function SignUpForm() {
@@ -11,41 +12,38 @@ export default function SignUpForm() {
     register,
     formState: { errors, isValid },
     handleSubmit,
-    getValues,
-  } = useForm<AuthFormType>({ mode: "onTouched" });
+  } = useForm<AuthFormType>({ resolver: zodResolver(signUpFormSchema), mode: "onTouched" });
 
   return (
     <form className="my-4 flex w-64 flex-col" onSubmit={handleSubmit(onSubmitSignUpForm)}>
       <FormInputField
         label="아이디"
-        register={register("userName", validationRules.username)}
+        register={register("userName")}
         error={errors.userName}
         inputType="text"
       />
       <FormInputField
-        label="이름"
-        register={register("name", validationRules.name)}
-        error={errors.name}
-        inputType="text"
-      />
-      <FormInputField
         label="휴대전화"
-        register={register("phoneNumber", validationRules.phoneNumber)}
+        register={register("phoneNumber")}
         error={errors.phoneNumber}
         inputType="text"
       />
       <FormInputField
+        label="이름"
+        register={register("name")}
+        error={errors.name}
+        inputType="text"
+      />
+
+      <FormInputField
         label="비밀번호"
-        register={register("password", validationRules.password)}
+        register={register("password")}
         error={errors.password}
         inputType="password"
       />
       <FormInputField
         label="비밀번호 재확인"
-        register={register("passwordConfirmation", {
-          ...validationRules.passwordConfirmation,
-          validate: (value) => validationRules.passwordConfirmation.validate(value, getValues),
-        })}
+        register={register("passwordConfirmation")}
         error={errors.passwordConfirmation}
         inputType="password"
       />
