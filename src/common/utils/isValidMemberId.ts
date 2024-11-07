669aec0c71d808e482e9d338e1a2be067a400391
dohyeons <ddhhss0603@gmail.com>
2024-11-07 15:45:51 +0900

    🔥 delete: isValidMemberId 삭제 #74

--- src/common/utils/isValidMemberId.ts
+++
@@ -1,4 +0,0 @@
-export default function isValidMemberId(memberId: number | null): memberId is number {
-  // 로그인이 되어있지 않을 경우 &&  && memberId가 3 보다 클 경우
-  return !memberId || memberId > 3 ? true : false;
-}
