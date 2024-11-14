b028097713080129e60889c07ec58b8679cde4fd
dohyeons <ddhhss0603@gmail.com>
2024-11-14 22:25:13 +0900

    ✨ feat: 유저정보 불러오는 엔드포인트 수정 #75

--- src/pages/MyPage/api/useGetMemberInfo.ts
+++ src/pages/MyPage/api/useGetMemberInfo.ts
@@ -1,10 +1,10 @@
 import axiosInstance from "@/common/utils/axiosInstance";
 import { useQuery } from "@tanstack/react-query";
 
-export default function useGetMemberInfo(memberId: number) {
+export default function useGetMemberInfo() {
   const { isLoading, data, isError } = useQuery({
-    queryKey: ["memberInfo", memberId],
-    queryFn: () => axiosInstance.get(`/members/${memberId}`).then((res) => res.data),
+    queryKey: ["memberInfo"],
+    queryFn: () => axiosInstance.get(`/members/me`).then((res) => res.data),
   });
 
   return { isLoading, data, isError };
