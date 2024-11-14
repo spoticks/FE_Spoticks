5a876c0580d412b0271326a1cb82dd463ec51698
dohyeons <ddhhss0603@gmail.com>
2024-11-14 15:20:33 +0900

    ✨ feat: 회원 정보 불러오고 초기화 로직 추가 #75

--- src/pages/MyPage/components/BasicUserInfo/BasicUserInfoForm.tsx
+++ src/pages/MyPage/components/BasicUserInfo/BasicUserInfoForm.tsx
@@ -1,27 +1,43 @@
 import { RED_BUTTON_STYLE_AUTH } from "@/common/buttonStyles";
 import BasicButton from "@/common/components/atoms/button/BasicButton";
+import Loading from "@/common/components/atoms/Loading";
 import FormInputField from "@/common/components/molecules/FormInputField";
-import useAuthStore from "@/common/stores/authStore";
 import { BasicInformation } from "@/common/types/formTypes";
 import validationRules from "@/common/validationRules";
+import ErrorPage from "@/pages/ErrorPage";
+import useGetMemberInfo from "@/pages/MyPage/api/useGetMemberInfo";
+import formatPhoneNumber from "@/pages/MyPage/utils/formatPhoneNumber";
+import { useEffect } from "react";
 import { useForm, SubmitHandler } from "react-hook-form";
 
-export default function BasicUserInfoForm() {
-  const { memberName, phoneNumber } = useAuthStore((state) => state);
-
+export default function BasicUserInfoForm({ memberId }: { memberId: number }) {
+  const { isLoading, data, isError } = useGetMemberInfo(memberId);
   const {
     register,
     formState: { errors, isValid },
     handleSubmit,
+    reset,
   } = useForm<BasicInformation>({
     mode: "onTouched",
     defaultValues: {
-      // 초기값 설정. 이름, 연락처, 이메일 값임.
-      phoneNumber: phoneNumber as string,
-      name: memberName as string,
+      // 초기값 설정.
+      phoneNumber: "",
     },
   });
+  useEffect(() => {
+    if (data) {
+      reset({
+        phoneNumber: formatPhoneNumber(data.phoneNumber),
+      });
+    }
+  }, [data, reset]);
 
+  if (isLoading) {
+    return <Loading />;
+  }
+  if (isError) {
+    return <ErrorPage />;
+  }
   const onSubmit: SubmitHandler<BasicInformation> = (data) => {
     // 유저 정보 변경 로직
     console.log(data);
