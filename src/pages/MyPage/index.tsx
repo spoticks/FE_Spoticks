4c042839204bb463d34f5686c1156a2b007b16d2
dohyeons <ddhhss0603@gmail.com>
2024-11-14 10:56:24 +0900

    ✨ feat: 회원 정보 페이지에서 회원 이름 렌더링 구현 #75

--- src/pages/MyPage/index.tsx
+++ src/pages/MyPage/index.tsx
@@ -1,16 +1,15 @@
 import AuthFirstHeading from "@/common/components/atoms/AuthFirstHeading";
-import useAuthStore from "@/common/stores/authStore";
+import useMemberInfo from "@/hooks/useMemberInfo";
 import BasicUserInfo from "@/pages/MyPage/components/BasicUserInfo";
 import PasswordResetting from "@/pages/MyPage/components/PasswordResetting";
 
 export default function MyPage() {
-  const { userName } = useAuthStore((state) => ({ userName: state.userName }));
-
+  const { memberName } = useMemberInfo();
   return (
     <>
       <div className="mb-10">
         <AuthFirstHeading content="회원정보" />
-        <span className="text-text-tertiary">어서오세요 {userName}(사용자 아이디) 님</span>
+        <span className="text-text-tertiary">어서오세요 {memberName} 님</span>
       </div>
       <div className="grid grid-cols-2">
         <BasicUserInfo />
