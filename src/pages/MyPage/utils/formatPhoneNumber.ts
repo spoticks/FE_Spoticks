5e961985eb2c2c1e3ba29a40032321f83f86e4b1
dohyeons <ddhhss0603@gmail.com>
2024-11-15 13:52:37 +0900

    🔥 delete: formatPhoneNumber삭제 #75

--- src/pages/MyPage/utils/formatPhoneNumber.ts
+++
@@ -1,6 +0,0 @@
-export default function formatPhoneNumber(phoneNumber: string) {
-  const matchedPhoneNumber = phoneNumber.match(/^(\d{3})(\d{4})(\d{4})$/);
-  return matchedPhoneNumber
-    ? `${matchedPhoneNumber[1]}-${matchedPhoneNumber[2]}-${matchedPhoneNumber[3]}`
-    : phoneNumber;
-}
