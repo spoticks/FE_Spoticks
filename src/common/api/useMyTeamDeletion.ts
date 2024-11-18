3207c5d0243eb7c2468d9c79b8e05d1886608db0
dohyeons <ddhhss0603@gmail.com>
2024-11-18 15:01:15 +0900

    ✨ feat: 마이팀 삭제 구현 #81

--- src/common/api/useMyTeamDeletion.ts
+++ src/common/api/useMyTeamDeletion.ts
@@ -1,8 +1,10 @@
+import alertToast from "@/common/utils/alertToast";
+import axiosInstance from "@/common/utils/axiosInstance";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
-import axios from "axios";
+import { AxiosError } from "axios";
 
 async function deleteTeam(teamId: string) {
-  await axios.delete(`http://localhost:3000/myteam/${teamId}`);
+  await axiosInstance.delete(`/myteam/${teamId}`);
 }
 export default function useMyTeamDeletion() {
   const queryClient = useQueryClient();
@@ -11,9 +13,19 @@ export default function useMyTeamDeletion() {
     mutationFn: deleteTeam,
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ["myTeam"] });
+      alertToast("성공적으로 팀을 제외했습니다!", "success");
     },
-    onError: (error) => {
-      console.log(error);
+    onError: (err: AxiosError) => {
+      if (err.response) {
+        const status = err.response?.status;
+        if (status === 403) {
+          alertToast("로그아웃 되었습니다! 다시 로그인해 주세요!", "error", "top");
+        }
+      } else if (err.request) {
+        alertToast("서버로부터 응답이 없습니다!", "error", "top");
+      } else {
+        alertToast("요청 중 문제가 발생했습니다!", "error", "top");
+      }
     },
   });
 
