b958a52bbc1dcd2af9b9394a426d00c56c537f7b
dohyeons <ddhhss0603@gmail.com>
2024-11-18 11:17:26 +0900

    ✨ feat: 마이 팀 페이지에서 회원 닉네임 불러오기 #81

--- src/pages/MyTeam/index.tsx
+++ src/pages/MyTeam/index.tsx
@@ -1,5 +1,5 @@
 import AuthFirstHeading from "@/common/components/atoms/AuthFirstHeading";
-import useAuthStore from "@/common/stores/authStore";
+import useMemberInfo from "@/hooks/useMemberInfo";
 import MyTeamList from "@/pages/MyTeam/components/MyTeamList";
 
 interface MyTeam {
@@ -7,14 +7,14 @@ interface MyTeam {
   teamName: string;
 }
 export default function MyTeam() {
-  const { userName } = useAuthStore((state) => ({ userName: state.userName }));
+  const { memberName } = useMemberInfo();
 
   return (
     <>
       <div className="mb-10 flex flex-col items-center border-b border-b-borders pb-2">
         <AuthFirstHeading content="My Team" />
         <span className="text-text-tertiary">
-          {userName}님이 관심있는 팀들의 일정을 확인해보세요!
+          {memberName}님이 관심있는 팀들의 일정을 확인해보세요!
         </span>
       </div>
       <MyTeamList />
