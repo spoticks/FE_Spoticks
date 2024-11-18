43cf829b967b469098cf1462d76a59a56dc5b070
dohyeons <ddhhss0603@gmail.com>
2024-11-18 15:13:01 +0900

    ✨ feat: MyTeamList 에서 EmptyTeamList 렌더링 및 기타 사항 구현 #81

--- src/pages/MyTeam/components/MyTeamList/index.tsx
+++ src/pages/MyTeam/components/MyTeamList/index.tsx
@@ -2,10 +2,9 @@ import useGetMyTeam from "@/common/api/useGetMyTeam";
 import Loading from "@/common/components/atoms/Loading";
 import ErrorPage from "@/pages/ErrorPage";
 import MyTeamCard from "@/pages/MyTeam/components/MyTeamList/MyTeamCard";
-
+import EmptyTeamList from "@/pages/MyTeam/components/MyTeamList/EmptyTeamList";
 export default function MyTeamList() {
   const { data, isLoading, isError } = useGetMyTeam();
-
   if (isLoading) {
     return <Loading />;
   }
@@ -15,11 +14,11 @@ export default function MyTeamList() {
   }
 
   return (
-    <section className="grid grid-cols-8 gap-[44px]">
+    <section className={`${data.length && "grid grid-cols-8 gap-[44px]"}`}>
       {data.length ? (
-        data.map((el) => <MyTeamCard key={el.teamId} teamName={el.teamName} teamId={el.teamId} />)
+        data.map((el) => <MyTeamCard key={el.id} teamName={el.teamName} id={el.id} />)
       ) : (
-        <div>아직 좋아하는 팀이 없어요...</div>
+        <EmptyTeamList />
       )}
     </section>
   );
