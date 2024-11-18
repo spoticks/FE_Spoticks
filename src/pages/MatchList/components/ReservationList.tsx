52882b0ede9400eced91159a1413dadbad15fcf1
devminoh <gmerit0116@naver.com>
2024-11-18 15:58:14 +0900

    ♻️ refactor: matchList page 더보기버튼 수정중 #70

--- src/pages/MatchList/components/ReservationList.tsx
+++ src/pages/MatchList/components/ReservationList.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { ContentProps } from "@/common/types/type";
 import { TimeButton } from "@/common/components/atoms/button/TimeButton";
@@ -6,60 +6,73 @@ import TdComp from "./ui/TdComp";
 import BasicButton from "@/common/components/atoms/button/BasicButton";
 interface ReservationListProps {
   filterData: ContentProps[];
+  setCurrentPage: React.Dispatch<React.SetStateAction<number>>;
+  totalEl: number;
 }
-const ReservationList = ({ filterData }: ReservationListProps) => {
-  const [viewMatches, setViewMatches] = useState(5);
+const ReservationList = ({ filterData, setCurrentPage, totalEl }: ReservationListProps) => {
+  const [viewMatches, setViewMatches] = useState<ContentProps[]>([]);
+
+  useEffect(() => {
+    // setViewMatches((prevMatches) => [...prevMatches, ...filterData]);
+    setViewMatches(filterData);
+  }, [filterData]);
 
   const addViewClick = () => {
-    setViewMatches((prev) => prev + 5);
+    if (totalEl > viewMatches.length) {
+      setCurrentPage((prevPage) => prevPage + 1);
+    }
   };
 
   const columnName = ["Home", "Away", "장소", "날짜", ""];
 
   return (
     <div>
-      <table className="mx-[-10px] w-full border-separate border-spacing-x-[10px]">
-        <thead>
-          <tr>
-            {columnName.map((column) => (
-              <th
-                key={column}
-                className={`border px-4 py-2 text-text-primary opacity-50 ${column === "" ? "border-none bg-none" : "bg-foreground"}`}
-              >
-                {column}
-              </th>
-            ))}
-          </tr>
-        </thead>
-        <tbody>
-          {filterData.slice(0, viewMatches).map((match, index) => (
-            <tr
-              key={match.gameId}
-              className={`${index % 2 === 0 ? "bg-gray-100" : "bg-white"} text-[20px]`}
-            >
-              <TdComp>{match.homeTeam}</TdComp>
-              <TdComp>{match.awayTeam}</TdComp>
-              <TdComp>{match.stadium}</TdComp>
-              <TdComp>{match.gameStartTime.split("T")[0]}</TdComp>
-              <TdComp>
-                <Link
-                  to={`/reservation/${match.gameId}`}
-                  state={{ match: match }}
-                  className="cursor-pointer"
+      {filterData.length > 0 ? (
+        <table className="mx-[-10px] w-full border-separate border-spacing-x-[10px]">
+          <thead>
+            <tr>
+              {columnName.map((column) => (
+                <th
+                  key={column}
+                  className={`border px-4 py-2 text-text-primary opacity-50 ${column === "" ? "border-none bg-none" : "bg-foreground"}`}
                 >
-                  <TimeButton
-                    timeOnSale={match.timeOnSale}
-                    timeOffSale={match.timeOffSale}
-                    label="예매하기"
-                    className="w-26 flex items-center justify-center rounded-[10px] px-8 py-1 text-xs text-foreground"
-                  />
-                </Link>
-              </TdComp>
+                  {column}
+                </th>
+              ))}
             </tr>
-          ))}
-        </tbody>
-      </table>
-      {viewMatches < filterData.length && (
+          </thead>
+          <tbody>
+            {viewMatches.map((match, index) => (
+              <tr
+                key={match.gameId}
+                className={`${index % 2 === 0 ? "bg-gray-100" : "bg-white"} text-[20px]`}
+              >
+                <TdComp>{match.homeTeam}</TdComp>
+                <TdComp>{match.awayTeam}</TdComp>
+                <TdComp>{match.stadium}</TdComp>
+                <TdComp>{match.gameStartTime.split("T")[0]}</TdComp>
+                <TdComp>
+                  <Link
+                    to={`/reservation/${match.gameId}`}
+                    state={{ match: match }}
+                    className="cursor-pointer"
+                  >
+                    <TimeButton
+                      timeOnSale={match.timeOnSale}
+                      timeOffSale={match.timeOffSale}
+                      label="예매하기"
+                      className="w-26 flex items-center justify-center rounded-[10px] px-8 py-1 text-xs text-foreground"
+                    />
+                  </Link>
+                </TdComp>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      ) : (
+        <h1>해당 팀의 경기가 없습니다!</h1>
+      )}
+      {totalEl > viewMatches.length && (
         <div className="mt-4 flex justify-center">
           <BasicButton
             content="더보기"
--- src/pages/MatchList/index.tsx
+++ src/pages/MatchList/index.tsx
@@ -18,47 +18,58 @@ export default function MatchList({ sport }: MatchListProps) {
   // Tab에서 선택된 team
   const [selectedTeam, setSelectedTeam] = useState("");
   const [filterData, setFilterData] = useState<ContentProps[]>([]);
-
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalEl, setTotatlEl] = useState(0);
+  console.log("filterData :", filterData);
+  console.log("currentPage :", currentPage);
   const {
     data: matchData = { content: [], pageInfo: {} as PageInfoProps },
     isLoading,
     error,
-  } = useMatchApi({ sport, selectedTeam: selectedTeam || "전체 일정" });
+  } = useMatchApi({ sport, selectedTeam: selectedTeam || "전체 일정", page: currentPage });
 
   useEffect(() => {
     setSelectedTeam("");
     navigate(`/match-list/${sport}`);
   }, [sport]);
-
+  console.log("data :", matchData);
   useEffect(() => {
-    if (selectedTeam === "전체 일정") {
-      setFilterData(matchData.content);
-      navigate(`/match-list/${sport}/allSche`);
-    } else {
-      const selectedTeamId = getTeamId(sport, selectedTeam);
-      if (selectedTeamId) {
-        setFilterData(
-          matchData.content.filter(
+    if (selectedTeam) {
+      if (selectedTeam === "전체 일정") {
+        setFilterData([]);
+        setCurrentPage(1);
+        setTotatlEl(matchData.pageInfo.totalElements);
+        setFilterData(matchData.content);
+        navigate(`/match-list/${sport}/allSche`);
+      } else {
+        const selectedTeamId = getTeamId(sport, selectedTeam);
+        if (selectedTeamId) {
+          const filteredMatches = matchData.content.filter(
             (game) => game.homeTeam === selectedTeam || game.awayTeam === selectedTeam,
-          ),
-        );
+          );
+          setFilterData(filteredMatches);
+        }
       }
     }
-  }, [matchData, selectedTeam, sport]);
+  }, [matchData, navigate, selectedTeam, sport]);
 
   if (isLoading) return <Loading />;
   if (error) return <Error />;
 
   return (
-    <div className="w-content-width flex flex-row pt-10">
-      {matchData.content.length > 0 && (
+    <div className="flex w-content-width flex-row pt-10">
+      {(matchData.content.length > 0 || (selectedTeam && filterData.length === 0)) && (
         <MatchListTab sport={sport} setSelectedTeam={setSelectedTeam} />
       )}
       <div className="flex w-full pl-[30px]">
-        {selectedTeam === "전체 일정" ? (
-          <MatchDetailMenu selectedTeam={selectedTeam} filterData={filterData} sport={sport} />
-        ) : selectedTeam ? (
-          <MatchDetailMenu selectedTeam={selectedTeam} filterData={filterData} sport={sport} />
+        {selectedTeam === "전체 일정" || selectedTeam ? (
+          <MatchDetailMenu
+            selectedTeam={selectedTeam}
+            filterData={filterData}
+            sport={sport}
+            setCurrentPage={setCurrentPage}
+            totalEl={totalEl}
+          />
         ) : (
           <Main sceduleLen={matchData.content.length} sport={sport} />
         )}
