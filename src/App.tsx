c613bff8a95677281705a52fc5480d457370b184
dohyeons <ddhhss0603@gmail.com>
2024-11-19 14:28:20 +0900

    ✨ feat: 전역 403 에러 설정 #84

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,24 @@
 import Router from "./Router";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { QueryCache, QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import useAuthStore from "@/common/stores/authStore";
+import { isAxiosError } from "axios";
+import alertToast from "@/common/utils/alertToast";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  queryCache: new QueryCache({
+    onError: (error) => {
+      // 리프레시 토큰이 제작된다면 로직 바꿀 것.
+      if (isAxiosError(error)) {
+        const status = error.response?.status;
+        if (status === 403) {
+          useAuthStore.getState().logout();
+          alertToast("로그인이 만료되었습니다. 다시 로그인 해주세요!", "info");
+        }
+      }
+    },
+  }),
+});
 
 function App() {
   return (
