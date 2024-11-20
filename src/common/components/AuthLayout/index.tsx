9dd9a12ae1cd27c865f85b9319cd9311848296c7
dohyeons <ddhhss0603@gmail.com>
2024-11-20 15:48:50 +0900

    ✨ feat: AuthLayout에 에러 바운더리 및 서스펜스 적용 #84

--- src/common/components/AuthLayout/index.tsx
+++ src/common/components/AuthLayout/index.tsx
@@ -1,7 +1,12 @@
 import AuthFirstHeading from "@/common/components/atoms/AuthFirstHeading";
 import MenuButton from "@/common/components/atoms/button/MenuButton";
+import Loading from "@/common/components/atoms/Loading";
 import useContent from "@/common/components/AuthLayout/hooks/useContent";
 import useAuthStore from "@/common/stores/authStore";
+import ErrorPage from "@/pages/ErrorPage";
+import { useQueryErrorResetBoundary } from "@tanstack/react-query";
+import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
 import { Navigate, Outlet } from "react-router-dom";
 
 const myTicketMenu = ["예매완료", "취소내역"];
@@ -10,6 +15,7 @@ export default function AuthRoute() {
     accessToken: state.accessToken,
   }));
   const { heading, paragraph } = useContent();
+  const { reset } = useQueryErrorResetBoundary();
   // 사용자가 로그인되지 않았으면 로그인 페이지로 리다이렉트
   if (!accessToken) {
     return <Navigate to="/login" />;
@@ -30,7 +36,15 @@ export default function AuthRoute() {
         )}
       </div>
       <hr className="border-1 mb-10 border-borders" />
-      <Outlet />
+      <Suspense fallback={<Loading />}>
+        <ErrorBoundary
+          FallbackComponent={ErrorPage}
+          onReset={reset}
+          resetKeys={[location.pathname]}
+        >
+          <Outlet />
+        </ErrorBoundary>
+      </Suspense>
     </>
   );
 }
