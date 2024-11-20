da139ac6e0ad06fb0e6f0fb2b80c68e3cbba62b7
dohyeons <ddhhss0603@gmail.com>
2024-11-20 15:40:24 +0900

    🔥 delete: 기존 헤딩 및 설명 삭제 #84

--- src/pages/AccountDeletion/index.tsx
+++ src/pages/AccountDeletion/index.tsx
@@ -1,15 +1,8 @@
 import CryingIcons from "@/assets/cryingface.svg?react";
-import AuthFirstHeading from "@/common/components/atoms/AuthFirstHeading";
 import AccountDeletionForm from "@/pages/AccountDeletion/components/AccountDeletionForm";
 export default function AccountDeletion() {
   return (
     <>
-      <div className="mb-10 border-b border-borders pb-5">
-        <AuthFirstHeading content="회원탈퇴" />
-        <span className="text-[20px] text-text-tertiary">
-          회원 탈퇴를 신청하기 전에 안내사항을 확인해주세요.
-        </span>
-      </div>
       <section className="flex">
         <span className="mr-8 text-[20px] text-text-tertiary">
           탈퇴 후 해당 아이디 정보로 로그인 할 수 없으며, 보유하신 혜택(포인트, 쿠폰 등)도 사용할 수
--- src/pages/MyPage/index.tsx
+++ src/pages/MyPage/index.tsx
@@ -1,23 +1,9 @@
-import AuthFirstHeading from "@/common/components/atoms/AuthFirstHeading";
-import useMemberInfo from "@/hooks/useMemberInfo";
 import BasicUserInfo from "@/pages/MyPage/components/BasicUserInfo";
 import PasswordResetting from "@/pages/MyPage/components/PasswordResetting";
 
 export default function MyPage() {
-  const { memberName, userName } = useMemberInfo();
   return (
     <>
-      <div className="mb-10">
-        <AuthFirstHeading content="회원정보" />
-        <p className="text-text-tertiary">
-          어서오세요
-          <strong>
-            {" "}
-            {memberName}({userName}){" "}
-          </strong>
-          님
-        </p>
-      </div>
       <div className="grid grid-cols-2">
         <BasicUserInfo />
         <PasswordResetting />
