f54ea1b387137afa9efc1b54ecd3c69e2f42ee38
dohyeons <ddhhss0603@gmail.com>
2024-11-20 16:45:15 +0900

    ✨ feat: mutationCache에 전역 403 에러 적용 #84

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import Router from "./Router";
-import { QueryCache, QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { MutationCache, QueryCache, QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import useAuthStore from "@/common/stores/authStore";
 import { isAxiosError } from "axios";
@@ -7,6 +7,18 @@ import alertToast from "@/common/utils/alertToast";
 
 const queryClient = new QueryClient({
   queryCache: new QueryCache({
+    onError: (error) => {
+      // 리프레시 토큰이 제작된다면 로직 바꿀 것.
+      if (isAxiosError(error)) {
+        const status = error.response?.status;
+        if (status === 403) {
+          useAuthStore.getState().logout();
+          alertToast("로그인이 만료되었습니다. 다시 로그인 해주세요!", "info");
+        }
+      }
+    },
+  }),
+  mutationCache: new MutationCache({
     onError: (error) => {
       // 리프레시 토큰이 제작된다면 로직 바꿀 것.
       if (isAxiosError(error)) {
