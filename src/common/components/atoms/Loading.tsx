a85aaf17a7252e442462b639635d6e8c4950dc8a
dohyeons <ddhhss0603@gmail.com>
2024-11-20 18:46:27 +0900

    💄 design: 로딩 컴포넌트 비율 수정 #84

--- src/common/components/atoms/Loading.tsx
+++ src/common/components/atoms/Loading.tsx
@@ -2,7 +2,7 @@ import Spinner from "@/assets/spinner.svg?react";
 
 export default function Loading() {
   return (
-    <div className="flex h-full w-full flex-col items-center justify-center">
+    <div className="flex flex-1 flex-col items-center justify-center">
       <Spinner />
       <h3>로딩중입니다.</h3>
     </div>
