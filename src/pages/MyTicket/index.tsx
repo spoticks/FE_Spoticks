48135ecd3c4f6c5b68b8ba75f0626e5f15fe5af1
dohyeons <ddhhss0603@gmail.com>
2024-11-21 11:04:19 +0900

    ✨ feat: div태그를 section으로 변경 #83

--- src/pages/MyTicket/index.tsx
+++ src/pages/MyTicket/index.tsx
@@ -5,13 +5,13 @@ export default function MyTicket() {
   const { data } = useMyTicketHistory();
 
   return (
-    <div className="grid grid-cols-3 flex-wrap gap-4">
+    <section className="grid grid-cols-3 flex-wrap gap-4">
       {/** InformationCard 혹은 정보 없음을 표시할 것. */}
       {data.length ? (
         data.map((el) => <InformationCard content={el} key={el.gameId} />)
       ) : (
         <span>조회 내역이 없습니다!</span>
       )}
-    </div>
+    </section>
   );
 }
