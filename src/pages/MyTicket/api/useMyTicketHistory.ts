b3b53840888ac2db7a86db405280c01effb2344c
dohyeons <ddhhss0603@gmail.com>
2024-11-22 11:32:14 +0900

    ✨ feat:useMyTicketHistory 인피니티 사용 x로 구현 #83

--- src/pages/MyTicket/api/useMyTicketHistory.ts
+++ src/pages/MyTicket/api/useMyTicketHistory.ts
@@ -1,16 +1,16 @@
-import { InformationCardProp } from "@/common/types/type";
+import { GameHistoryType } from "@/common/types/type";
 import axiosInstance from "@/common/utils/axiosInstance";
 import { useSuspenseQuery } from "@tanstack/react-query";
 import { useLocation } from "react-router-dom";
 
 export default function useMyTicketHistory() {
   const location = useLocation().pathname;
-  const param = location === "/profile/my-tickets/my-reservations" ? "COMPLETE" : "CANCEL";
-  const { data } = useSuspenseQuery<InformationCardProp[]>({
+  const param = location.includes("/profile/my-tickets/my-reservations") ? "COMPLETE" : "CANCEL";
+  const { data } = useSuspenseQuery<GameHistoryType>({
     queryKey: ["myReservations", param],
     queryFn: async () => {
       const res = await axiosInstance.get(`reservation?status=${param}&page=1`);
-      return res.data;
+      return res.data.content;
     },
   });
   return {
