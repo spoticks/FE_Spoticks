bd7833be7c27a592d9089ebff8a8abbbd12e8ea9
dohyeons <ddhhss0603@gmail.com>
2024-11-22 11:35:16 +0900

    ✨ feat: MyTicket key 수정 #83

--- src/pages/MyTicket/index.tsx
+++ src/pages/MyTicket/index.tsx
@@ -4,12 +4,11 @@ import NoTicketHistory from "@/pages/MyTicket/components/NoTicketHistory";
 
 export default function MyTicket() {
   const { data } = useMyTicketHistory();
-
   return (
-    <section className={`${data?.length ? "grid grid-cols-8 gap-4" : "flex flex-1"}`}>
+    <section className={`${data?.length ? "grid grid-cols-3 gap-4" : "flex flex-1"}`}>
       {/** InformationCard 혹은 정보 없음을 표시할 것. */}
       {data.length ? (
-        data.map((el) => <InformationCard content={el} key={el.gameId} />)
+        data.map((el) => <InformationCard content={el} key={el.reservationId} />)
       ) : (
         <NoTicketHistory />
       )}
