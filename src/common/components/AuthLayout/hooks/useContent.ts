81407c1dbd65b5864fce98fcaedf7befb912b917
dohyeons <ddhhss0603@gmail.com>
2024-11-22 11:30:54 +0900

    🔥 delete: 필요없는 console.log 삭제 #83

--- src/common/components/AuthLayout/hooks/useContent.ts
+++ src/common/components/AuthLayout/hooks/useContent.ts
@@ -3,6 +3,7 @@ import { useLocation } from "react-router-dom";
 
 export default function useContent() {
   const location = useLocation();
+  console.log(location.pathname);
   const { heading, paragraph } = getContent(location.pathname);
 
   return { heading, paragraph };
