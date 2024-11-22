4806003a40159d6f9c16b19a28b659452a79f458
dohyeons <ddhhss0603@gmail.com>
2024-11-22 11:30:02 +0900

    💄 design: DefaultCard css 수정 #83

--- src/common/components/atoms/DefaultCard.tsx
+++ src/common/components/atoms/DefaultCard.tsx
@@ -1,6 +1,6 @@
 export default function DefaultCard({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex-1 rounded-[15px] border border-borders bg-foreground p-3 shadow-first">
+    <div className="flex-1 rounded-[10px] border border-borders bg-foreground p-3 shadow-first">
       {children}
     </div>
   );
