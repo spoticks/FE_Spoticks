b925a6f5c86b6af13bd6da3c5d56276162c90c0a
dohyeons <ddhhss0603@gmail.com>
2024-11-25 14:21:57 +0900

    ✨ feat: useReservationDetails에 예약 상태와 경기 날짜와 현재 날짜 비교 계산 추가 #83

--- src/pages/MyTicket/api/useReservationDetails.ts
+++ src/pages/MyTicket/api/useReservationDetails.ts
@@ -18,14 +18,28 @@ interface GameReservation {
 }
 
 const fetchReservationDetails = async (reservationId: number) => {
-  console.log(reservationId);
   const { data } = await axiosInstance.get(`/reservation/${reservationId}`);
   return data;
 };
 
+function getReservationStatus(reservationStatus: string) {
+  return reservationStatus === "COMPLETED" ? true : false;
+}
+
+function isGameDatePast(gameStartTime: string) {
+  const today = new Date();
+  today.setHours(0, 0, 0, 0);
+  const gameDate = new Date(gameStartTime);
+  gameDate.setHours(0, 0, 0, 0);
+  // 게임 시작일 <= 오늘
+  return gameDate <= today;
+}
 export default function useReservationDetails(reservationId: number) {
-  return useSuspenseQuery<GameReservation>({
+  const { data, isSuccess } = useSuspenseQuery<GameReservation>({
     queryKey: ["reservationDetail", reservationId],
     queryFn: () => fetchReservationDetails(reservationId),
   });
+  const isReservationComplete = getReservationStatus(data.reservationStatus);
+  const gamePassed = isGameDatePast(data.game.gameStartTime);
+  return { data, isSuccess, isReservationComplete, gamePassed };
 }
