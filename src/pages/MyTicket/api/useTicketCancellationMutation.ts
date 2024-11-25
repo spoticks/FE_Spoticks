36dbcf08a89cf1ebed25c80eaa3b4fbfef51d3ee
dohyeons <ddhhss0603@gmail.com>
2024-11-25 14:56:23 +0900

    ✨ feat:  useTicketCancellationMutation 성공 시 예약 페이지로 이동 추가 #83

--- src/pages/MyTicket/api/useTicketCancellationMutation.ts
+++ src/pages/MyTicket/api/useTicketCancellationMutation.ts
@@ -1,9 +1,11 @@
 import alertToast from "@/common/utils/alertToast";
 import axiosInstance from "@/common/utils/axiosInstance";
 import { useQueryClient, useMutation } from "@tanstack/react-query";
+import { useNavigate } from "react-router-dom";
 
 export default function useTicketCancellationMutation(onClose: () => void, reservationId: number) {
   const queryClient = useQueryClient();
+  const navigate = useNavigate();
   const mutation = useMutation({
     mutationFn: async (reservationId: number) => {
       await axiosInstance.delete(`reservation/${reservationId}`);
@@ -11,10 +13,10 @@ export default function useTicketCancellationMutation(onClose: () => void, reser
     onSuccess: () => {
       alertToast("예약을 취소했습니다!", "success");
       queryClient.invalidateQueries({ queryKey: ["myReservations"] });
+      navigate("/profile/my-tickets/my-reservations");
       onClose();
     },
     onError: (error) => {
-      console.log(error);
       alertToast("취소에 실패했습니다!", "error");
     },
   });
