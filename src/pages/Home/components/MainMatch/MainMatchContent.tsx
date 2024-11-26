5ac5a90db8568b93ef96665f0c85db3aa3c63542
dohyeons <ddhhss0603@gmail.com>
2024-11-26 15:27:29 +0900

    ✨ feat: MainMatchContent 컴포넌트 구현 #90

---
+++ src/pages/Home/components/MainMatch/MainMatchContent.tsx
@@ -0,0 +1,24 @@
+import useMainMatch from "@/pages/Home/api/useMainMatch";
+import MainMatchInfo from "@/pages/Home/components/MainMatch/MainMatchInfo";
+import TicketOffSale from "@/pages/Home/components/MainMatch/TicketOffSale";
+import Timer from "@/pages/Home/components/MainMatch/Timer";
+import isSaleTimeOff from "@/pages/Home/utils/isSaleTimeOff";
+
+export default function MainMatchContent() {
+  const {
+    data: { homeTeam, awayTeam, timeOffSale, timeOnSale, gameId },
+  } = useMainMatch();
+  const isTicketOnSale = isSaleTimeOff(timeOnSale, timeOffSale);
+  return (
+    <>
+      {isTicketOnSale ? <Timer mainMatchStartTime={timeOffSale} /> : <TicketOffSale />}
+      <MainMatchInfo
+        homeTeam={homeTeam}
+        awayTeam={awayTeam}
+        gameId={gameId}
+        timeOffSale={timeOffSale}
+        timeOnSale={timeOnSale}
+      />
+    </>
+  );
+}
