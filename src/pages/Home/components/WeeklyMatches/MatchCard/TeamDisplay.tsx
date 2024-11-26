1a8f8c552e93ba372358cd15f0c495464b63b6c9
dohyeons <ddhhss0603@gmail.com>
2024-11-26 15:29:01 +0900

    ✨ feat: TeamDisplay 컴포넌트 태그 변경 #90

--- src/pages/Home/components/WeeklyMatches/MatchCard/TeamDisplay.tsx
+++ src/pages/Home/components/WeeklyMatches/MatchCard/TeamDisplay.tsx
@@ -4,7 +4,7 @@ export default function TeamDisplay({ teamName }: { teamName: string }) {
   return (
     <div className="flex flex-col items-center gap-2">
       <TeamLogo teamName={teamName} />
-      <span className="w-16 text-center text-[16px] font-semibold">{teamName}</span>
+      <p className="w-16 truncate text-center text-[16px] font-semibold">{teamName}</p>
     </div>
   );
 }
