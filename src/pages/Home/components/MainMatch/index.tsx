2e5ee2488bbe66c2d84283a2cfe13b7c25f0a9db
dohyeons <ddhhss0603@gmail.com>
2024-11-26 13:49:48 +0900

    ✨ feat: MainMatch 컴포넌트 api 연결 #90

--- src/pages/Home/components/MainMatch/index.tsx
+++ src/pages/Home/components/MainMatch/index.tsx
@@ -1,27 +1,41 @@
+import useMainMatch from "@/pages/Home/api/useMainMatch";
 import MainMatchInfo from "@/pages/Home/components/MainMatch/MainMatchInfo";
+import TicketOffSale from "@/pages/Home/components/MainMatch/TicketOffSale";
 import Timer from "@/pages/Home/components/MainMatch/Timer";
+import isSaleTimeOff from "@/pages/Home/utils/isSaleTimeOff";
 
-const mainMatch = {
-  // 임시 정보
-  content: [
-    {
-      gameId: 56,
-      homeTeamName: "한화 이글스",
-      awayTeamName: "두산 베어스",
-      sportName: "야구",
-      gameStartTime: "2024-09-04T15:40:20",
-    },
-  ],
-};
-
+export interface MainMatchType {
+  awayTeam: string;
+  gameId: number;
+  gameStartTime: string;
+  homeTeam: string;
+  sport: string;
+  stadium: string;
+  timeOffSale: string;
+  timeOnSale: string;
+}
 export default function MainMatch() {
+  const {
+    data: { homeTeam, awayTeam, timeOffSale, timeOnSale, gameId },
+  } = useMainMatch();
+  const isTicketOnSale = isSaleTimeOff(timeOnSale, timeOffSale);
+
   return (
-    <div className="flex flex-col gap-9">
-      <h1 className="text-2xl font-bold">
-        What is the most popular <span className="text-red-500">match</span>?
-      </h1>
-      <Timer mainMatchStartTime={mainMatch.content[0].gameStartTime} />
-      <MainMatchInfo matchData={mainMatch.content[0]} />
+    <div className="flex w-[620px] flex-col items-center gap-9">
+      <div>
+        <h1 className="text-2xl font-bold">
+          이번주 <strong className="text-red-500">인기 매치</strong> 입니다!
+        </h1>
+      </div>
+
+      {isTicketOnSale ? <Timer mainMatchStartTime={timeOffSale} /> : <TicketOffSale />}
+      <MainMatchInfo
+        homeTeam={homeTeam}
+        awayTeam={awayTeam}
+        gameId={gameId}
+        timeOffSale={timeOffSale}
+        timeOnSale={timeOnSale}
+      />
     </div>
   );
 }
