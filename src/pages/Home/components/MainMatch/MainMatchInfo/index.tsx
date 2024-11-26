0a9e557077b2e959cb94928e0d870bb757ff5a5b
dohyeons <ddhhss0603@gmail.com>
2024-11-26 13:52:21 +0900

    ✨ feat: MainMatchInfo 컴포넌트 수정 #90

--- src/pages/Home/components/MainMatch/MainMatchInfo/index.tsx
+++ src/pages/Home/components/MainMatch/MainMatchInfo/index.tsx
@@ -1,14 +1,34 @@
-import Button from "@/common/components/atoms/Button";
+import BasicButton from "@/common/components/atoms/button/BasicButton";
+import LinkButton from "@/common/components/atoms/button/LinkButton";
+import { MainMatchType } from "@/pages/Home/components/MainMatch";
 import MainMatchTeam from "@/pages/Home/components/MainMatch/MainMatchInfo/MainMatchTeam";
-import { MatchData } from "@/type";
+import isSaleTimeOff from "@/pages/Home/utils/isSaleTimeOff";
 
-export default function MainMatchInfo({ matchData }: { matchData: MatchData }) {
+export default function MainMatchInfo({
+  homeTeam,
+  awayTeam,
+  gameId,
+  timeOffSale,
+  timeOnSale,
+}: Pick<MainMatchType, "homeTeam" | "awayTeam" | "gameId" | "timeOffSale" | "timeOnSale">) {
+  const isTicketOnSale = isSaleTimeOff(timeOnSale, timeOffSale);
+
   return (
-    <div className="flex items-center gap-4 rounded border border-borders bg-foreground p-4">
-      <MainMatchTeam teamName={matchData.homeTeamName} isHome />
-      <span className="font-extrabold">VS</span>
-      <MainMatchTeam teamName={matchData.awayTeamName} />
-      <Button content="예매하기" />
+    <div className="flex w-full flex-col items-center justify-center rounded border border-borders bg-foreground p-4">
+      <MainMatchTeam homeTeam={homeTeam} awayTeam={awayTeam} />
+      {isTicketOnSale ? (
+        <LinkButton
+          content="예매하기"
+          linkTo={`/reservation/${gameId}`}
+          style="w-full px-3 py-1 rounded-[10px] bg-Accent text-[16px] text-center text-foreground hover:bg-button-hovered disabled:cursor-not-allowed disabled:bg-disabled-button"
+        />
+      ) : (
+        <BasicButton
+          content="예매불가"
+          disabled
+          style="w-full px-3 py-1 rounded-[10px] text-[16px] text-center text-foreground disabled:cursor-not-allowed disabled:bg-disabled-button"
+        />
+      )}
     </div>
   );
 }
