2cb65ce610beee27d48d570f71e6a9636e552c76
dohyeons <ddhhss0603@gmail.com>
2024-11-27 16:10:31 +0900

    ✨ feat: 403에러 핸들링 구현 #93

--- src/hooks/usehandleAuthError.ts
+++ src/hooks/usehandleAuthError.ts
@@ -3,14 +3,18 @@ import useAuthStore from "@/common/stores/authStore";
 import alertToast from "@/common/utils/alertToast";
 import getErrorToastMessage from "@/common/utils/getErrorToastMessage";
 import { isAxiosError } from "axios";
+import { useNavigate } from "react-router-dom";
 
 export default function useHandleAuthError(error: Error) {
+  const navigate = useNavigate();
   if (isAxiosError(error)) {
     const status = error.response?.status;
     const message = error.response?.data.message;
     if (status === 401) {
       useAuthStore.getState().logout();
       queryClient.clear();
+    } else if (status === 403) {
+      navigate(-1);
     }
     const toastMessage = getErrorToastMessage(message);
     alertToast(toastMessage, "error");
