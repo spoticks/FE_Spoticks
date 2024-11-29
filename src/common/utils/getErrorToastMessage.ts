d9e93871fd364fa6f3a5950fbe5ac1f5c141ab64
dohyeons <ddhhss0603@gmail.com>
2024-11-29 14:13:27 +0900

    ✨ feat: getErrorToastMessage에 케이스 추가 #93

--- src/common/utils/getErrorToastMessage.ts
+++ src/common/utils/getErrorToastMessage.ts
@@ -1,4 +1,5 @@
 export default function getErrorToastMessage(message: string) {
+  console.log(message);
   switch (message) {
     case "JWT Token expired":
       return "로그인이 만료되었습니다. 다시 로그인 해주세요!";
@@ -8,7 +9,9 @@ export default function getErrorToastMessage(message: string) {
       return "인증 토큰 형식이 잘못되었습니다. 다시 로그인해 주세요.";
     case "Access is Denied":
       return "접근 권한이 없습니다!";
+    case "Invalid username or password":
+      return "로그인해주세요!";
     default:
-      return "로그아웃 되었습니다! 다시 로그인해 주세요!";
+      return "알 수 없는 에러가 발생했습니다!";
   }
 }
