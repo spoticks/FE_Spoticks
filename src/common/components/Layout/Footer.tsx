fc40c4eb1b5d6440973b9a67a68f03937b8edab5
dohyeons <ddhhss0603@gmail.com>
2024-12-02 16:20:12 +0900

    ✨ feat: 프로젝트 깃허브 링크 클릭 구현 완료 #95

--- src/common/components/Layout/Footer.tsx
+++ src/common/components/Layout/Footer.tsx
@@ -1,7 +1,22 @@
+import GithubLogo from "@/assets/github-mark.svg?react";
+
 export default function Footer() {
+  function openGithubLink(url: string) {
+    window.open(url, "_blank");
+  }
   return (
-    <footer className="flex h-[80px] w-full justify-center border">
-      <div className="w-content-width flex items-center">Footer</div>
+    <footer className="flex h-[160px] w-full justify-center bg-slate-400">
+      <div className="flex w-content-width items-center justify-center">
+        <button
+          className="flex items-center gap-1 text-[16px] font-semibold transition-colors duration-200 hover:text-text-tertiary"
+          onClick={() => {
+            openGithubLink("https://github.com/orgs/spoticks/repositories");
+          }}
+        >
+          <GithubLogo className="size-5" />
+          Spoticks on Github
+        </button>
+      </div>
     </footer>
   );
 }
