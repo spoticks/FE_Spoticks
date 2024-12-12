52a3e49756d5df4a3e6621b6a4c00312612268ae
dohyeons <ddhhss0603@gmail.com>
2024-12-13 01:13:03 +0900

    ✨ feat: 리프레시 토큰을 사용한 재로그인 구현 #99

--- src/common/utils/axiosInstance.ts
+++ src/common/utils/axiosInstance.ts
@@ -1,4 +1,5 @@
 import useAuthStore from "@/common/stores/authStore";
+import alertToast from "@/common/utils/alertToast";
 import axios from "axios";
 
 const axiosInstance = axios.create({
@@ -16,4 +17,37 @@ axiosInstance.interceptors.request.use((config) => {
   return config;
 });
 
+axiosInstance.interceptors.response.use(
+  (res) => res,
+  async (err) => {
+    const originalRequest = err.config;
+    const errorMessage = err.response?.data.message;
+    if (
+      errorMessage === "JWT Token expired" &&
+      !originalRequest._retry &&
+      !originalRequest.url.includes("/auth/reissue")
+    ) {
+      originalRequest._retry = true;
+      try {
+        const userName = useAuthStore.getState().userName;
+        const { accessToken } = useAuthStore.getState();
+        const axiosRefreshInstance = axios.create({
+          baseURL: "http://spoticks.shop:8080/",
+          timeout: 3000,
+          headers: { "Content-Type": "application/json", Authorization: `Bearer ${accessToken}` },
+          withCredentials: true,
+        });
+        const response = await axiosRefreshInstance.post(`/auth/reissue/${userName}`);
+        const { token } = response.data;
+        useAuthStore.getState().login(token);
+        originalRequest.headers.Authorization = `Bearer ${token}`;
+        return axiosInstance(originalRequest);
+      } catch (err) {
+        useAuthStore.getState().logout();
+        alertToast("다시 로그인 해주세요!", "error");
+      }
+    }
+    return Promise.reject(err);
+  },
+);
 export default axiosInstance;
