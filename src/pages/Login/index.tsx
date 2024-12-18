614b16181e9522808c61bee0c58772b02e2b97e2
dohyeons <ddhhss0603@gmail.com>
2024-12-18 15:23:47 +0900

    ✨ feat: 회원가입, 로그인에 로고 추가 #93

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,10 +1,12 @@
 import LoginForm from "@/pages/Login/components/LoginForm";
 import AuthFirstHeading from "@/common/components/atoms/AuthFirstHeading";
 import LinkButton from "@/common/components/atoms/button/LinkButton";
+import AppLogo from "@/common/components/atoms/AppLogo";
 
 export default function Login() {
   return (
     <section className="flex w-full flex-col items-center pt-12">
+      <AppLogo style="size-11 pb-2" />
       <AuthFirstHeading content="어서오세요!" />
       <span>
         아직 회원이 아니신가요?{" "}
