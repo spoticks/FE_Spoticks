32c98eb1aa58b263dd913f33287109a952c2d919
dohyeons <ddhhss0603@gmail.com>
2024-12-19 20:36:05 +0900

    ✨ feat: Header 유저 버튼 렌더링 조건 변경 #93

--- src/common/components/Layout/Header/index.tsx
+++ src/common/components/Layout/Header/index.tsx
@@ -3,16 +3,17 @@ import AuthButtonGroup from "./AuthButtonGroup";
 import UserButton from "./UserButton";
 import AppLogo from "@/common/components/atoms/AppLogo";
 import useMemberInfo from "@/hooks/useMemberInfo";
+import useAuthStore from "@/common/stores/authStore";
 
 export default function Header() {
   const { authority } = useMemberInfo();
-
+  const { userName } = useAuthStore((state) => state);
   return (
     <header className="sticky top-0 z-50 flex h-[80px] w-full justify-center border bg-foreground">
       <div className="flex w-content-width items-center justify-between">
         <AppLogo />
         {authority !== "ROLE_ADMIN" && <HeaderNav />}
-        {!authority.length ? <AuthButtonGroup /> : <UserButton />}
+        {!userName ? <AuthButtonGroup /> : <UserButton />}
       </div>
     </header>
   );
