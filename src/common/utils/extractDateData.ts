5e1764b9aa9c1e4c580f2411e3e884bdc316539e
devminoh <gmerit0116@naver.com>
2025-01-02 20:22:03 +0900

    ♻️ refactor: 오픈예정버튼 변경 #70

--- src/common/utils/extractDateData.ts
+++ src/common/utils/extractDateData.ts
@@ -1,10 +1,11 @@
 export default function extractDateData(rawTime: string) {
   const [date, time] = rawTime.split("T");
   const formattedDate = new Date(date);
-  const month = formattedDate.getMonth() + 1;
-  const day = formattedDate.getDate();
+  const year = formattedDate.getFullYear() % 100;
+  const month = String(formattedDate.getMonth() + 1).padStart(2, "0");
+  const day = String(formattedDate.getDate()).padStart(2, "0");
   const [hours, minutes] = time.split(":");
   const daysOfWeek = ["일", "월", "화", "수", "목", "금", "토"];
   const weekday = daysOfWeek[formattedDate.getDay()];
-  return { date, month, day, weekday, hours, minutes };
+  return { year, date, month, day, weekday, hours, minutes };
 }
