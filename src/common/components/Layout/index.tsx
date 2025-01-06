bb8b0c28e9ad458d80ce48d2ccddbf3371799cb2
dohyeons <ddhhss0603@gmail.com>
2025-01-06 17:50:47 +0900

    💄 design: Layout 길이 설정 수정 #93

--- src/common/components/Layout/index.tsx
+++ src/common/components/Layout/index.tsx
@@ -10,10 +10,11 @@ export default function Layout() {
   return (
     <div className="flex min-h-screen flex-col items-center">
       {!shouldExcludeHeaderFooter && <Header />}
-      <div className="relative flex w-content-width flex-1 flex-col items-center pb-20 pt-[80px]">
+      <div className="relative flex w-full max-w-[1280px] flex-1 flex-col items-center pb-20 pt-[80px]">
         <main className="flex w-full flex-1 flex-col">
           <Outlet />
         </main>
+        {/*배경 빨간얼룩 */}
         {!shouldExcludeHeaderFooter && <Vector className="fixed left-0 top-[-20px] -z-50" />}
       </div>
       {!shouldExcludeHeaderFooter && <Footer />}
