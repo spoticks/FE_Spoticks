c6d59b2ac01c353b735babef0e8b0fedba78e62d
dohyeons <ddhhss0603@gmail.com>
2025-02-22 20:59:57 +0900

    feat: 관리자 계정은 / 대신 /admin으로 보내기

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,8 +1,16 @@
 import MainMatch from "@/pages/Home/components/MainMatch";
 import Tickets from "@/assets/Tickets.svg?react";
 import WeeklyMatches from "@/pages/Home/components/WeeklyMatches";
+import useMemberInfo from "@/hooks/useMemberInfo";
+import { Navigate } from "react-router-dom";
 
 export default function Home() {
+  const authority = useMemberInfo();
+
+  if (authority === "ROLE_ADMIN") {
+    return <Navigate to="/admin" />;
+  }
+
   return (
     <div className="mb-24">
       <section className="my-12 flex items-center justify-between text-center">
